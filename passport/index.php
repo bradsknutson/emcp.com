26ce7667edc6b9c60144565e13b1fa6c7278ec4e
Brad Knutson <brad.s.knutson@gmail.com>
2014-08-12 15:31:41 -0500

    Passport page initial push

--- passport/index.php
+++ passport/index.php
@@ -3,10 +3,40 @@
 	include '../includes/header.php';
 ?>
 	<div id="container">
+        <div class="vimeo-modal"></div>
+        <div class="header-hero-hover-2"></div>
 		<div class="header-secondary-page"></div>
 		<div class="header-top-dark">
 			<?php include '../includes/nav.php'; ?>
 		</div>
-
+        <div class="fofih">
+            <div class="fofih-container">
+                <h2>The future of fluency is here.</h2>
+                <div class="play-button">
+					<div class="icon-play"></div>
+				</div>
+            </div>
+            <div class="ninehundred textcenter">                
+                <p><strong>Passport&reg;</strong> is a revolutionary world language learning environment that allows teachers to assign classwork, track student progress, and easily report results to parents or school districts – all within a single application and easily accessible with one click. It seamlessly delivers EMC's leading world language content and assets in a research-based platform developed by the University of Minnesota (LT Media Lab).<br />With <strong>Passport&reg;</strong> students are immersed in their target language and culture, while teachers capture and measure progress with performance-based tasks and assessments.</p>
+                
+                <img src="<?php echo $server; ?>lib/images/discover.png" alt="Discover" />
+                <p><strong>Discover</strong> &mdash; Teachers and students explore award-winning curricula, accessible anytime, anywhere, and from any device.</p>
+                
+                <img src="<?php echo $server; ?>lib/images/create.png" alt="Create" />
+                <p><strong>Create</strong> &mdash; Teachers assign pre-made, IPA-aligned projects that engage students in real-life situations using the three modes of communication—Interpretive, Interpersonal and Presentational.</p>
+                
+                <img src="<?php echo $server; ?>lib/images/expand.png" alt="Expand" />
+                <p><strong>Expand</strong> &mdash; Students immerse themselves in the culture and language they're studying with continuously updated news articles, videos, and songs.</p>
+                
+                <img src="<?php echo $server; ?>lib/images/share.png" alt="Share" />
+                <p><strong>Share</strong> &mdash; Teachers and students communicate in and out of the classroom with a unique video forum in which teachers post questions and students respond through recorded videos.</p>
+                
+                <img src="<?php echo $server; ?>lib/images/perform.png" alt="Perform" />
+                <p><strong>Perform</strong> &mdash; Teachers assess students' oral proficiency through customizable assessments.</p>
+                
+                <img src="<?php echo $server; ?>lib/images/report.png" alt="Report" />
+                <p><strong>Report</strong> your students' progress on activities, assessments, and standards throughout the year with customizable, easy-to-export graphs and charts.</p>
+            </div>
+        </div>
 	
 <?php include '../includes/footer.php'; ?>
