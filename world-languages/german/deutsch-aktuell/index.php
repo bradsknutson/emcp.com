86aeca69c704411888737464ab9705f8336efb54
Brad Knutson <brad.s.knutson@gmail.com>
2014-08-26 13:55:24 -0500

    Initial Deutsch Aktuell Commit

---
+++ world-languages/german/deutsch-aktuell/index.php
@@ -0,0 +1,154 @@
+ <?php 
+	$custom_title = 'Deutsch Aktuell | ';
+	include '../../../includes/header.php';
+?>
+	<div id="container" class="deutsch-aktuell">
+		<div class="vimeo-modal"></div>
+		<div class="header-background-image da-bg">
+			<div id="topImg">            
+			</div>
+			<div class="colorBar" style="background:#c10006; height:10px; width:100%; margin-top:-37px; position:relative; top:35px;"></div>
+		</div>
+		<div class="header-top">
+			<?php include '../../../includes/nav.php'; ?>
+		</div>
+		<div class="secondary-first">
+			<div class="ninesixty">
+				<div class="conectate-left">
+					<span class="bold"><span class="italic">Deutsch Aktuell</span></span> will engage your students and launch them on a path to life-long fluency.  This 3-level German program focuses on experiences and activities involving German, Austrian, and Swiss teenagers and adults. We created <span class="italic">Deutsch Aktuell</span> with the most up-to-date research in teaching and learning German, and our program is intended for students with varying abilities, backgrounds, interests, and learning styles. Multiplatform eBooks that work on any device, video-based teaching tools, authentic cultural videos, ACTFL-aligned proficiency assessments, and much more—it’s all here waiting to be discovered!
+				</div>
+				<div class="conectate-right">
+					<img src="<?php echo $server; ?>lib/images/da-cover.jpg" alt="Deutsch Aktuell German Program" />
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+        <div class="qc-passport-pre">
+            <div class="ninesixty marginbottom40">
+                <h2>Available on&nbsp;</h2>
+                <img src="<?php echo $server; ?>lib/images/passport-logo-400.png" alt="Passport" />            
+            </div>
+        </div>
+		<div class="emc-technology-bg passport-tech-bg" style="margin-top:-40px;"></div>		
+		<div class="emc-languages">
+			<div class="ninehundred no-top">
+				<div class="emc-languages-learn">
+					<div class="ell-left">
+						<div class="quote">
+							<span class="red-quotes">"</span>The future of fluency depends on building student competency through engaging performance.<span class="red-quotes">"</span>
+						</div>
+						<div class="quote-author">
+							<strong>- Eric Cantor</strong>, CEO<br />New Mountain Learning
+						</div>
+					</div>
+					<div class="ell-right">
+						<p>Through a longterm collaborative partnership with the LT Media Lab at the University of Minnesota, we are building, integrating, and researching the future of fluency. <span class="bold">Passport&trade;</span> builds upon and extends research-based language learning platforms created by the LT Media Lab by leveraging EMC Publishing's world language instructional content in an easy-to-use environment designed to drive educator effectiveness and student fluency. Students will discover languages and cultures, expand their knowledge beyond the textbook and written test, and perform what they've learned within a portfolio-style environment to achieve lasting proficiency. </p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>			
+		<div class="redefining">
+			<div class="ninehundred no-bottom">
+				<div class="aligncenter">
+					<h2>Redefining seamless learning through technology.</h2>
+				</div>
+				
+				<p>Through our partnership with the University of Minnesota LT Media Lab, we are now offering award-winning, research-based learning environments for your face-to-face, blended, or online classroom.</p>
+			</div>
+			<div class="redefining-bg">
+				<div class="vimeo-modals-container">
+					<div class="avenue-play-button">
+						<div class="play-button">
+							<div class="icon-play"></div>
+						</div>
+					</div>
+					<div class="avenue-play-button-hover-background"></div>
+					<div class="flipgrid-play-button">
+						<div class="play-button">
+							<div class="icon-play"></div>
+						</div>
+					</div>
+					<div class="flipgrid-play-button-hover-background"></div>
+				</div>            
+            </div>
+			<div class="ninesixty">
+				<div class="avenue-flipgrid">
+					<div class="one-half first">
+						<p><span class="bold">Avenue&trade;</span> is a simple, yet powerful e-assessment platform where your students build portfolios of their spoken performances. Educators can create custom tasks or build tasks based on EMC's rich, pre-loaded library of media.</p>
+						
+						<a href="http://emcl.com/avenue" target="_blank" class="learn-more-link">Learn more about Avenue</a>
+					</div>
+					<div class="one-half last">
+						<p><span class="bold">Flipgrid&trade;</span> is a video-based discussion environment where students respond to short, teacher-created questions. Flipgrid brings the back row to the front in your classroom.</p>
+						
+						<a href="http://emcl.com/flipgrid" target="_blank" class="learn-more-link">Learn more about Flipgrid</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+        <div class="qc-tech da-tech">
+			<div class="ninesixty onesix fonteighteen">
+				<h3 class="adonde">Multiplatform eBooks</h3>
+				<p>Our eBooks connect students with the language in and out of the classroom. With a touch of the finger, students access every page as well as all of the audio (vocabulary presentations, dialogues and activities). Students also have all of the video resources at their fingertips, both dialogue and continuous storyline videos.</p>
+				
+				<p>Our eBooks are available for delivery on all laptops, desktops, and mobile devices including the iPad®. The best part is that you can have our multiplatform eBook for one low price!</p>
+                
+                <p><a href="http://emc.bookshelf.emcp.com" target="_blank" class="learn-more-link">Learn more about eBooks</a></p>
+			</div>
+		</div>                
+		<div class="conectate-iculture">
+			<div class="ninesixty">
+				<div class="conectate-iculture-first">
+                    <div class="vimeo-modals-container">
+                        <div class="iculture-play-button">
+                            <div class="play-button">
+                                <div class="icon-play"></div>
+                            </div>
+                        </div>
+                        <div class="iculture-play-button-hover-background"></div>
+                    </div> 					
+                    <img src="<?php echo $server; ?>lib/images/iculture-laptop.png" alt="i-Culture" />
+				</div>
+				<div class="conectate-iculture-second">
+                    <h3 class="conectate">i-Culture</h3>
+					<p class="no-top">Watch authentic culture come alive for your students as they immerse themselves in engaging and varied media: videos, news articles, and songs. i-Passport, i-Videos, and i-Songs bring cultural experiences into the classroom and into your students'  lives.</p>
+
+					<p class="no-bottom"><a href="http://store.emcp.com/i-culture" class="learn-more-link">Learn more about i-Culture</a></p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="redefining">
+			<div class="redefining-bg-2">
+				<div class="vimeo-modals-container">
+					<div class="dvd-play-button">
+						<a href="https://vimeo.com/103283691" class="elm-video fancybox-media">
+                            <div class="play-button">
+                                <div class="icon-play"></div>
+                            </div>
+                        </a>
+					</div>
+					<div class="dvd-play-button-hover-background"></div>
+				</div>                    
+            </div>
+			<div class="ninesixty">
+				<div class="avenue-flipgrid conectate">
+					<div class="one-half first">
+						<h3 class="conectate">DVD Program: <span class="italic">Treffpunkt Berlin</span></h3>
+						
+						<p>Filmed on location in Berlin with professional actors and actresses, <span class="italic">Treffpunkt Berlin</span> features a continuous, intriguing story line woven throughout the three-level video series. Your students will cling to the edge of their seats as each episode of this unique video series unfolds! Can love blossom at the hottest café in Berlin? Stay tuned…</p>
+					</div>
+					<div class="one-half last">
+						<h3 class="conectate">ELPAC</h3>
+						
+						<p>Use EMC Language Proficiency Assessments by CARLA* (ELPAC) (performance-based, standardized assessments) to measure your students’ Spanish, French, or German language proficiency. Choose from multi-level online assessments in Reading, Listening, Writing, and Speaking that can be administered individually or as a battery. Developed with input from language teachers and school administrators, ELPAC assessments meet the highest standards for reliability while offering flexibility and ease in delivery.</p>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>		
+
+	
+<?php include '../../../includes/footer.php'; ?>
