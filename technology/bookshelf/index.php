1ae2f7cf7df42fb82169b7badf32c90f971873d5
Brad Knutson <brad.s.knutson@gmail.com>
2014-08-29 15:22:47 -0500

    Initial Bookshelf Commit

---
+++ technology/bookshelf/index.php
@@ -0,0 +1,49 @@
+<?php 
+	$custom_title = 'Bookshelf | Multiplatform eBooks | ';
+	include '../../includes/header.php';
+?>
+	<div id="container" class="bookshelf">
+		<div class="header-background-image bookshelf-bg">
+			<div id="topImg">
+                <div class="tech-button-container">
+                    <a class="technology-buttons bookshelf-demo transition" href="http://emc.bookshelf.emcp.com" target="_blank">Demo</a>
+                    <a class="technology-buttons bookshelf-login transition" href="http://emc.bookshelf.emcp.com" target="_blank">Login</a>
+                </div>
+            </div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="secondary-first">
+			<div class="ninesixty">
+                <div class="featured-left">
+                    <h2>Multiplatform eBooks take learning, well, anywhere</h2>
+                    <p>EMC's multi-platform eBooks connect students to language program resources in and out of the classroom. One-touch navigation allows them to connect to every page of their textbook and workbooks as well as all audio content. Multi-platform eBooks work on all laptops, desktops, and mobile devices including the iPad&copy;</p>
+                </div>
+			</div>
+		</div>
+        <div class="white marginbottom40">
+            <div class="ninesixty">
+                <div class="left-block">
+                    <p><span class="p-larger">With EMC’s Multi-platform eBooks students*…</span></p>
+                    <ul>
+                        <li>View digital textbook pages and complete practice activities interactively </li>
+                        <li>Receive immediate feedback</li>
+                        <li>Listen to abundant audio by native speakers that includes vocabulary presentations, dialogues, listening activities, and authentic readings</li>
+                    </ul>
+                    <p><span class="p-smaller">*Not all features are available for every title.</span></p>
+                </div>
+                <div class="right-block">
+                    <ul>
+                        <li>Watch full-length continuous storyline and cultural dialogue videos, recorded by native speakers</li>
+                        <li>Print activities to use in class</li>
+                        <li>E-mail completed activities to the teacher</li>
+                        <li>Export, view and print pages as PDFs</li>
+                        <li>Access a full glossary of vocabulary to reference for each unit</li>
+                    </ul>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	
+<?php include '../../includes/footer.php'; ?>
