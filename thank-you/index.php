17276d9f87a2800ded29ba64e05ffbf3c065c15a
Brad Knutson <brad.s.knutson@gmail.com>
2015-03-20 15:25:51 -0500

    Thank You Page Template initial commit

---
+++ thank-you/index.php
@@ -0,0 +1,16 @@
+<?php 
+	$custom_title = 'Technology | ';
+	include '../includes/header.php';
+?>
+	<div id="container" class="technology">
+		<div class="header-secondary-page"></div>
+		<div class="header-top-dark">
+			<?php include '../includes/nav.php'; ?>
+		</div>
+		<div class="world-languages-title">
+			<div class="ninehundred onesix fonttwenty">
+				<h3>Thank you!</h3>
+			</div>
+		</div>
+		
+<?php include '../includes/footer.php'; ?>		
---
+++ thank-you/white-paper/index.php
@@ -0,0 +1,22 @@
+<?php 
+	$custom_title = 'Thank You! | ';
+	include '../../includes/header.php';
+?>
+	<div id="container" class="mw">
+		<div class="vimeo-modal"></div>
+		<div class="header-background-image thank-you-page">
+			<div id="topImg">
+                <h1>Thank You!</h1>
+                <a class="technology-buttons ty-download transition" href="http://www.emcp.com/lib/assets/5SecretsToWinningGrants.pdf" target="_blank" onClick="ga('send', 'event', 'Call To Action Button','click','Thank You Download');">Download Your White Paper</a>
+			</div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="secondary-first ty-page">
+			<div class="ninesixty">                
+                <p>Thank you for downloading 5 Secrets to Winning Grants. Please visit <a href="/">emcp.com</a> for more information about our products.</p>
+			</div>
+		</div>
+	
+<?php include '../../includes/footer.php'; ?>
