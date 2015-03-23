4dd387faf81a719c75e8b43302a9da0b22d16f0c
Brad Knutson <brad.s.knutson@gmail.com>
2015-03-23 13:46:35 -0500

    NCEA15 Contest Page

---
+++ contest/ncea15/index.php
@@ -0,0 +1,79 @@
+<?php 
+	$custom_title = 'Contest | ';
+	include '../../includes/header.php';
+?>
+	<div id="container" class="contest">
+		<div class="header-background-image contest-ncea15-bg">
+			<div id="topImg">
+                <div class="tech-button-container"></div>
+            </div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="contest-first">
+			<div class="ninesixty">
+                <div class="featured-left" style="float:left;">
+					<img style="margin-top:15px;" src="../../lib/images/passport-text.jpg" border="0" />
+					<p style="font-size:20px;">EMC Publishing will be giving away one $20,000 Digital Classroom prize on Thursday, April 9th during the final exhibit hour of the National Catholic Educational Association Conference!</p>
+					<p style="font-size:20px;">Visit EMC's booth, #412, during all open exhibit hours of the conference to enter to win. One lucky teacher will leave the conference a hero, bringing back 3 classroom sets of textbooks PLUS some of the most cutting-edge technology for their World Language Department!</p>
+                </div>
+				<div class="featured-left" style="float:left; margin:40px 0 0 40px;">
+					<img src="../../lib/images/contest-passport.jpg" border="0" />
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div>
+			<div class="ninesixty">
+				<div style="width:100%;">
+					<h3 style="font-size:20px;"><strong>What's Passport&reg;?</strong></h3>
+				</div>
+                <div class="featured-left" style="float:left;">
+                    <p style="margin-top:10px;font-size:20px;">Passport&reg; is a learning environment uniquely designed for the world language classroom. Students connect to curriculum and so much more! 
+					Passport's innovative tools immerse students in the culture and provide opportunities for them to create projects, share with classmates, and perform what they know.</p>
+					<br />
+					<h3 style="font-size:20px;margin-bottom:0;"><strong>Passport Features</strong></h3>
+                </div>
+				<div class="featured-left" style="float:left; margin-left:40px;">
+					<img src="../../lib/images/passport-device-agnostic.jpg" border="0" />
+				</div>
+				<div class="clearfix"></div>
+				<div>
+					<div style="float:left;width:35%;font-size:20px;">
+						<p>&#8226; performance-based assessment<br />
+						&#8226; automatic grading and feedback<br />
+						&#8226; gradebook<br />
+						&#8226; class management</p>
+					</div>
+					<div style="float:left;width:28%;font-size:20px;">
+						<p>&#8226; assigning<br />
+						&#8226; interactive eBooks<br />
+						&#8226; project-based learning<br />
+						&#8226; cultural immersion</p>
+					</div>
+					<div style="float:left;width:37%;font-size:20px;">
+						<p>&#8226; video-based sharing<br />
+						&#8226; differentiated personalized learning</p>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+        <div class="light-grey">
+			<div class="ninesixty" style="padding:8px 0 50px;">
+				<h3><strong>Eligibility and rules:</strong></h3>
+				<p>
+					<ul>
+						<li>Must enter at EMC's booth, #412.</li>
+						<li>The official drawing will take place at 1pm Thursday, April 9th durign the final exhibit hour. You must be present at EMC's booth at hte time of the drawing to claim your prize.</li>
+						<li>The winner will have 5 minutes to claim their prize, before we move on and draw another name.</li>
+						<li>Winner may choose 1 world language in Passport and 1 world language textbook program for up to 3 classes or 150 students.</li>
+						<li>Valid for the 2015-16 school year.</li>
+					</ul>
+				</p>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	
+<?php include '../../includes/footer.php'; ?>
