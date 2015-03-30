85f72ee63f887f89d8775097b2b39c19a5a8d81c
weinerdog <bmolina@emcp.com>
2015-03-30 16:56:13 -0500

    NCEA edits

--- contest/ncea15/index.php
+++ contest/ncea15/index.php
@@ -13,11 +13,9 @@
 		</div>
 		<div class="contest-first">
 			<div class="ninesixty">
-                <div class="featured-left" style="float:left;">
-					<h2 style="font-size:20px;margin-bottom:0;color:#BC0406;"><strong>EMC's NCEA 2015 Classroom Upgrade YOUR WAY Contest!</strong></h2>
-					<p style="font-size:20px;">EMC Publishing will be giving away one Classroom Upgrade (the winner's choice) on Thursday, April 9th during the National Catholic Educational Association Conference!</p>
-					<p style="font-size:20px;">Visit EMC's booth, #412, to enter. One lucky conference goer will leave a hero, bringing back THEIR CHOICE of the following 3 prizes...</p>
-                </div>
+				<h2 style="font-size:24px;margin-bottom:0;color:#BC0406;"><strong>EMC's NCEA 2015 Classroom Upgrade YOUR WAY Contest!</strong></h2>
+				<p style="font-size:20px;">EMC Publishing will be giving away one Classroom Upgrade (the winner's choice) on Thursday, April 9th during the National Catholic Educational Association Conference!
+				Visit EMC's booth, #412, to enter. One lucky conference goer will leave a hero, bringing back THEIR CHOICE of the following 3 prizes...</p>
 			</div>
 			<div class="clearfix"></div>
 		</div>
@@ -29,7 +27,7 @@
 					<h3 style="font-size:20px;margin-bottom:0;"><strong>For world language and English language arts classrooms</strong> ($10,000 value)</h3>
                     <p style="margin-top:10px;font-size:20px;">
 						<ul style="margin-top:0;">
-							<li><strong>The Passport&reg;</strong> learning environment for up to 3 classes. Passport's innovative tools provide opportunities for students 
+							<li><strong>The Passport&reg; learning environment</strong> for up to 3 classes. Passport's innovative tools provide opportunities for students 
 								to create projects, share with classmates, and perform what they know.</li>
 							<li>Professional development to get you off on the right foot</li>
 						</ul>
@@ -41,15 +39,15 @@
 				<div class="clearfix"></div>
 		<!-- For World Language Elementary Classrooms -->
                 <div class="featured-left" style="float:left;">
-					<h3 style="font-size:20px;margin-bottom:0;"><strong>For world language elementary classrooms</strong> ($6,000 value)</h3>
+					<h3 style="font-size:20px;margin:15px 0 0;"><strong>For world language elementary classrooms</strong><br />($6,000 value)</h3>
                     <p style="margin-top:10px;font-size:20px;">
 						<ul style="margin-top:0;">
 							<li>3 sets of 100 French and Spanish picture dictionary books full of games plus comprehension and coloring activities.</li>
-							<li>A complete Symtalk<sup style="font-size:7px;">TM</sup> Spanish or French program for 3 classes</li>
+							<li>A complete <strong>Symtalk<sup style="font-size:7px;">TM</sup> Spanish or French program</strong> for 3 classes</li>
 						</ul>
 					</p>
                 </div>
-				<div class="featured-left" style="float:left; margin-left:40px;">
+				<div class="featured-left" style="float:left; margin-left:50px;">
 					<img src="../../lib/images/contest-symtalk.jpg" border="0" />
 				</div>
 				<div class="clearfix"></div>
@@ -58,10 +56,10 @@
 					<h3 style="font-size:20px;margin-bottom:0;"><strong>For any classroom!</strong> ($3,000 value)</h3>
                     <p style="margin-top:10px;font-size:20px;">
 						<ul style="margin-top:0;">
-							<li>Flipgrid<sup style="font-size:7px;">TM</sup> video sharing and collaboration tool for up to 3 classes. In Flipgrid<sup style="font-size:7px;">TM</sup>, teachers 
+							<li><strong>Flipgrid<sup style="font-size:7px;">TM</sup></strong> video sharing and collaboration tool for up to 3 classes. In Flipgrid<sup style="font-size:7px;">TM</sup>, teachers 
 								create grids of short questions (using text or video) that students respond to through recorded videos.
 							</li>
-							<li>Avenue<sup style="font-size:7px;">TM</sup> video assessment for up to 3 classes. Avenue<sup style="font-size:7px;">TM</sup> is an innovative e-assessment platform 
+							<li><strong>Avenue<sup style="font-size:7px;">TM</sup></strong> video assessment for up to 3 classes. Avenue<sup style="font-size:7px;">TM</sup> is an innovative e-assessment platform 
 								for building custom tasks to capture, evaluate, archive, and visualize progress of video-based learner performances in any classroom.
 							</li>
 						</ul>
@@ -76,12 +74,7 @@
         <div class="light-grey">
 			<div class="ninesixty" style="padding:8px 0 50px;">
 				<h3><strong>Eligibility and rules:</strong></h3>
-				<p>Must enter at EMC's booth, #412. The official drawing will take place at 1pm Thursday, April 9th 
-				during the final exhibit hour. You must be present at EMC's booth at the time of the drawing to claim your prize.
-				The winner will have 5 minutes to claim their prize, before we move on and draw another name. Winner may choose 1 
-				world language in Passport and 1 world language textbook program for up to 3 classes or 150 students. Valid for 
-				the 2015-16 school year.
-				</p>
+				<p>Must enter at EMC's booth, #412. The official drawing will take place April 9th. You do not need to be present at the time of the drawing in order to win. Valid for the 2015-16 school year.</p>
 			</div>
 			<div class="clearfix"></div>
 		</div>
