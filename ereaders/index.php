5c5d2c9f62bf48923f275bbb053a9a51b5e6f2b3
Brad Knutson <brad.s.knutson@gmail.com>
2015-04-14 09:42:57 -0500

    eReaders page updates

--- ereaders/index.php
+++ ereaders/index.php
@@ -20,103 +20,110 @@
                 
                 <div class="clearfix"></div>
                 
-                <img class="ereaders-right-img" src="<?php echo $server; ?>lib/images/iPadeReaders-image.jpg" alt="eReader Features" style="margin:30px 30px 0 0;" />
-                
-                <p class="ereaders-larger">eReader Featured</p>
-                
-                <ul class="ereaders-smaller">
-                    <li>beautifully illustrated</li>
-                    <li>synchronized audio</li>
-                    <li>notes</li>
-                    <li>interactive activities</li>
-                    <li>games</li>
-                </ul>
-                
-                <table>
-                    <tr>
-                        <td>
-                            <table cellspacing="0">
-                                <tr>
-                                    <td colspan="2">
-                                        <p class="ereaders-largest">Titles Available</p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td valign="top">
-                                        <p class="ereaders-smaller"><em>CANTAR DEL MIO CID<br />
-                                        CARMEN<br />
-                                        CASA DE BERNARDA ALBA<br />
-                                        CONJUNTO ESPECIAL<br />
-                                        CUENTOS FANTASTICOS<br />
-                                        EL SECRETO DE ROMINA 2012<br />
-                                        FORMULA SECRETA<br />
-                                        INDAGACIONES MADRID<br />
-                                        LAZARILLO D.TORMES<br />
-                                        ZORRO 2013<br />
-                                        EL COLLAR VISIGODO<br />
-                                        MISTERIO EN EL MALLORCA GRAN HOTEL<br />
-                                        GUZMAN DE ALFARANCHE<br />
-                                        DON QUIJOTE DE LA MANCHA NE<br />
-                                        DAS HAUS DEN KLIPPEN<br />
-                                        DIE NACHBARN<br />
-                                        DIE VERWANDLUNG<br />
-                                            EINE SPEZIELLE BAND</em></p>
-                                    </td>
-                                    <td valign="top">
-                                        <p class="ereaders-smaller"><em>MORD IN GRAND HOTEL 2013<br />
-                                        DIEBSTAHL IN HAMBURG<br />
-                                        DAS HAUS AN DEN KLIPPEN NE<br />
-                                        DER GOLEM<br />
-                                        JOAHNN WOLFGANG GOETHE<br />
-                                        DEUX ANS VACANCES<br />
-                                        ERIC A DISPARU<br />
-                                        FANTOME DE L'OPERA 2013<br />
-                                        LA BELLE ET LA BETE<br />
-                                        MISERABLES<br />
-                                        MOMIE DU LOUVRE<br />
-                                        NOTRE-DAME DE PARIS<br />
-                                        POURSUITE DANS PARIS<br />
-                                        SECRET DE LOUISE 2012<br />
-                                        VENGEANCE LA REUNION<br />
-                                        A LA RECHERCHE DE TIBOU<br />
-                                        AVENTURE DANS LES PYRENEES<br />
-                                        PARIS EST EN DANGER<br />
-                                        APARITION ET AUTRES CONTES</em></p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="2">
-                                        <p class="ereaders-largest">Pricing</p>
-                                    </td>
-                                </tr>
-                                <tr class="border-bottom-tr">
-                                    <td align="left"><p class="ereaders-smaller">Single 1-year license</p></td>
-                                    <td align="right"><p class="ereaders-smaller">$2.95</p></td>
-                                </tr>
-                                <tr class="border-bottom-tr">
-                                    <td align="left" class="whitespace-top"><p class="ereaders-smaller">15 student class library 1-year license</p></td>
-                                    <td align="right"><p class="ereaders-smaller">32.95</p></td>
-                                </tr>
-                                <tr class="border-bottom-tr">
-                                    <td align="left"><p class="ereaders-smaller">30 student class library 1-year license</p></td>
-                                    <td align="right"><p class="ereaders-smaller">62.95</p></td>
-                                </tr>
-                                <tr class="border-bottom-tr">
-                                    <td align="left" class="whitespace-top"><p class="ereaders-smaller">15 student class library 6-year license</p></td>
-                                    <td align="right"><p class="ereaders-smaller">197.95</p></td>
-                                </tr>
-                                <tr class="border-bottom-tr">
-                                    <td align="left"><p class="ereaders-smaller">30 student class library 6-year license</p></td>
-                                    <td align="right"><p class="ereaders-smaller">375.95</p></td>
-                                </tr>
-                            </table>
-                        </td>
-                        <td>
-                            <img src="<?php echo $server; ?>lib/images/TallReaders-image.jpg" alt="eReaders" style="margin-left:70px;" />
-                        </td>
-                    </tr>
-                </table>
-                
+            </div>
+            <div class="redefining light-grey-slice">
+                <div class="ninesixty">
+                    
+                    <img class="ereaders-right-img" src="<?php echo $server; ?>lib/images/iPadeReaders-image.png" alt="eReader Features" style="margin:50px 50px 0 0;" />
+                    
+                    <p class="ereaders-larger">eReader Features</p>
+
+                    <ul class="ereaders-smaller">
+                        <li>beautifully illustrated</li>
+                        <li>synchronized audio</li>
+                        <li>notes</li>
+                        <li>interactive activities</li>
+                        <li>games</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="redefining">
+                <div class="ninesixty">
+                    <table>
+                        <tr>
+                            <td>
+                                <table cellspacing="0">
+                                    <tr>
+                                        <td colspan="2">
+                                            <p class="ereaders-largest">Titles Available</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td valign="top" style="padding: 0 20px 0;">
+                                            <p class="ereaders-smaller"><em>CANTAR DEL MIO CID<br />
+                                            CARMEN<br />
+                                            CASA DE BERNARDA ALBA<br />
+                                            CONJUNTO ESPECIAL<br />
+                                            CUENTOS FANTASTICOS<br />
+                                            EL SECRETO DE ROMINA 2012<br />
+                                            FORMULA SECRETA<br />
+                                            INDAGACIONES MADRID<br />
+                                            LAZARILLO D.TORMES<br />
+                                            ZORRO 2013<br />
+                                            EL COLLAR VISIGODO<br />
+                                            MISTERIO EN EL MALLORCA GRAN HOTEL<br />
+                                            GUZMAN DE ALFARANCHE<br />
+                                            DON QUIJOTE DE LA MANCHA NE<br />
+                                            DAS HAUS DEN KLIPPEN<br />
+                                            DIE NACHBARN<br />
+                                            DIE VERWANDLUNG<br />
+                                                EINE SPEZIELLE BAND</em></p>
+                                        </td>
+                                        <td valign="top">
+                                            <p class="ereaders-smaller"><em>MORD IN GRAND HOTEL 2013<br />
+                                            DIEBSTAHL IN HAMBURG<br />
+                                            DAS HAUS AN DEN KLIPPEN NE<br />
+                                            DER GOLEM<br />
+                                            JOAHNN WOLFGANG GOETHE<br />
+                                            DEUX ANS VACANCES<br />
+                                            ERIC A DISPARU<br />
+                                            FANTOME DE L'OPERA 2013<br />
+                                            LA BELLE ET LA BETE<br />
+                                            MISERABLES<br />
+                                            MOMIE DU LOUVRE<br />
+                                            NOTRE-DAME DE PARIS<br />
+                                            POURSUITE DANS PARIS<br />
+                                            SECRET DE LOUISE 2012<br />
+                                            VENGEANCE LA REUNION<br />
+                                            A LA RECHERCHE DE TIBOU<br />
+                                            AVENTURE DANS LES PYRENEES<br />
+                                            PARIS EST EN DANGER<br />
+                                            APARITION ET AUTRES CONTES</em></p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">
+                                            <p class="ereaders-largest">Pricing</p>
+                                        </td>
+                                    </tr>
+                                    <tr class="border-bottom-tr">
+                                        <td align="left"><p class="ereaders-smaller">Single 1-year license</p></td>
+                                        <td align="right"><p class="ereaders-smaller">$2.95</p></td>
+                                    </tr>
+                                    <tr class="border-bottom-tr">
+                                        <td align="left" class="whitespace-top"><p class="ereaders-smaller">15 student class library 1-year license</p></td>
+                                        <td align="right"><p class="ereaders-smaller">$32.95</p></td>
+                                    </tr>
+                                    <tr class="border-bottom-tr">
+                                        <td align="left"><p class="ereaders-smaller">30 student class library 1-year license</p></td>
+                                        <td align="right"><p class="ereaders-smaller">$62.95</p></td>
+                                    </tr>
+                                    <tr class="border-bottom-tr">
+                                        <td align="left" class="whitespace-top"><p class="ereaders-smaller">15 student class library 6-year license</p></td>
+                                        <td align="right"><p class="ereaders-smaller">$197.95</p></td>
+                                    </tr>
+                                    <tr class="border-bottom-tr">
+                                        <td align="left"><p class="ereaders-smaller">30 student class library 6-year license</p></td>
+                                        <td align="right"><p class="ereaders-smaller">$375.95</p></td>
+                                    </tr>
+                                </table>
+                            </td>
+                            <td>
+                                <img src="<?php echo $server; ?>lib/images/TallReaders-image.jpg" alt="eReaders" style="margin-left:70px;" />
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
 	
