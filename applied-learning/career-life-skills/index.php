9a557cb89722174d6789e637e2992525173ebcaa
Brad Knutson <brad.s.knutson@gmail.com>
2015-04-16 13:11:44 -0500

    Updates per Matthias to AL Pages

--- applied-learning/career-life-skills/index.php
+++ applied-learning/career-life-skills/index.php
@@ -39,25 +39,12 @@
 		<div class="white-slice">
 			<div class="ninesixty">
                 <div class="left-slice">
-                    <img src="<?php echo $server; ?>lib/images/al-cs-preparing.png" alt="Preparing for Career Success" />
+                    <img src="<?php echo $server; ?>lib/images/al-cs-developing-career.png" alt="Developing Career & Living Skills" />
                 </div>
                 <div class="right-slice">
-                    <h1>Preparing for Career Success</h1>
-                    <p><em>Preparing for Career Success</em> (for grades 10-12) provides students with teh most current career information and throroughly covers a variety of social issues affecting today's workers and the workplace, including tech preparation, ethics in the workplace, sexual harassment, and the complex set of problem-solving skills needed to successfully handle the challenges facing two-wage-earner families.</p>
-                    <a class="learn-more-link" href="<?php echo $server; ?>applied-learning/career-life-skills/preparing-for-career-success/">Learn more</a>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-		<div class="grey-slice">
-			<div class="ninesixty slice-odd">
-                <div class="left-slice">
                     <h1>Developing Career & Living Skills</h1>
                     <p><em>Developing Career and Living Skills</em> equips students with skills and attitudes necessary for a successful high school career and transition into postsecondary and workplace environments by investigating trends in careers, career opportunities, and life skills. </p>
                     <a class="learn-more-link" href="<?php echo $server; ?>applied-learning/career-life-skills/developing-career-living-skills/">Buy It Now!</a>
-                </div>
-                <div class="right-slice">
-                    <img src="<?php echo $server; ?>lib/images/al-cs-developing-career.png" alt="Developing Career & Living Skills" />
                 </div>
                 <div class="clearfix"></div>
             </div>
