44a9cfa5f4d30276065cf959557e3102ce165643
weinerdog <bmolina@emcp.com>
2015-05-13 15:38:15 -0500

    EMC Georgia Landing Page

---
+++ georgia-overview.php
@@ -0,0 +1,274 @@
+<?php include 'includes/header.php'; ?>
+	<div id="container" class="main">
+		<div style="text-align:center;"><img src="/lib/images/georgia/Ga-WL-Landing-header.jpg" border="0" usemap="#header" /></div>
+		<!--
+		<div class="header-top">
+			<?php //include 'includes/nav.php'; ?>
+		</div>
+		-->
+	<!-- Que Chevere -->
+		<div style="text-align:center;"><a name="qc"></a><img src="/lib/images/georgia/GA-QC-header.jpg" border="0" /></div>
+		<div>
+			<div class="ninehundred">
+				<h3>Program Resources</h3>
+				<strong>Student</strong>
+				<ul>
+					<li>Textbook (print)</li>
+					<li>eBook (digital)</li>
+					<li>Workbook (print and digital)</li>
+					<li>Grammar &amp; Vocabulary Exercises (print and digital)</li>
+					<li>Listening Activities (digital)</li>
+				</ul>
+				<strong>Teachers</strong>
+				<ul>
+					<li>Annotated Teacher's Edition (print) includes margin notes featuring games and activities, cultural information, and teacher suggestions.</li>
+					<li>eATE (digital, powered by Bookshelf&reg;) includes the full Annotated Teacher's Edition plus all of the following program resources.
+						<ul>
+							<li>Textbook MP3 Audio includes all recordings from the textbook: dialogues, narratives, and activities.</li>
+							<li>Textbook Audio Program Manual includes the audio transcripts for the textbook audio.</li>
+							<li>Workbook Teacher's Edition includes the answers to the student activities that reinforce vocabulary, grammar, and cultural information.</li>
+							<li>Grammar and Vocabulary Exercises Teacher's Edition includes the answers to the student activities that reinforce grammar and vocabulary.</li>
+							<li>Listening Activities with MP3 Audio includes additional listening comprehension activities as well as the audio and answer key.</li>
+							<li>Communicative Activities Manual includes information gap activities, postcard activities, and situation card activities.</li>
+							<li>e-Visuals are images and scenes to reinforce vocabulary. Some e-visuals include audio.</li>
+							<li>TPR Storytelling Manual includes step-by-step instructions for basic and advanced stories.</li>
+							<li>Materiales para hispanohablantes includes activities for heritage speakers. Also included for heritage learners (in print only) is the reader Marcelino pan y vino.</li>
+							<li>Portfolio Assessment includes rubrics to assess project-based learning activities.</li>
+							<li>Juegos are interactive games that correlate to the vocabulary and grammar for each lesson.</li>
+							<li>Video Program: El cuarto misterioso is a continuous storyline video that reinforces each unit's vocabulary and grammar. A corresponding manual includes transcripts, student activity sheets, and an answer key.</li>
+							<li>Quizzes with Answer Key includes several lesson quizzes for each unit as well as the answer key.</li>
+							<li>Test Booklet with Answer Key and MP3 Audio includes one test per unit to assess speaking, listening, reading and writing based on unit vocabulary, grammar, and culture.</li>
+							<li>ExamVIEW&reg; is software preloaded with the unit tests from the Test Booklet as well as the unit Quizzes. These assessments can be adapted, printed, and administered to students.</li>
+						</ul>
+					</li>
+				</ul>
+				<strong>Passport</strong>
+				<p><img src="/lib/images/georgia/Passport-Device-Agnostic-QC.jpg" border="0" style="float:right;margin-left:10px;" />
+					Enhance teaching and increase student engagement with the learning environment designed for tomorrow's World Language classroom, Passport&reg;.<br /><br />
+					Passport&copy; "brings it all home" with its unique anytime, anywhere learning platform. In the classroom, at home, on the go, students connect to curriculum, culture, 
+					classmates, and communities as well as showing what they know. Immerse your students in culture and language as well as capture and measure their progress with Passport's 
+					performance-based tasks and project-based learning.
+				</p>
+				<div>
+					<div style="float:left;">
+						<p>Passport&copy; features...
+						<ul style="margin-left:-27px;">
+							<li>&iexcl;Qu&eacute; ch&eacute;vere! eBooks <br />
+							(textbook, workbook, listening activities,<br />
+							grammar and vocabulary exercises)</li>
+							<li>project-based learning</li>
+							<li>cultural immersion</li>
+							<li>video-based sharing</li>
+							<li>performance-based assessment</li>
+							<li>automatic grading and feedback</li>
+							<li>gradebook</li>
+							<li>class management</li>
+							<li>assigning</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	<!-- Tes Branche -->
+		<div style="text-align:center;"><a name="teb"></a><img src="/lib/images/georgia/GA-TEB-header.jpg" border="0"></div>
+		<div>
+			<div class="ninehundred">
+				<div>
+					<h3>T'es branch&eacute;?</h3>
+					<strong>Program Resources</strong><br /><br />
+
+					<strong>Student</strong>
+					<ul>
+						<li>Textbook (print)</li>
+						<li>eBook  (digital)</li>
+						<li>Workbook (print and digital)</li>
+						<li>Listening Activities (digital)</li>
+					</ul>
+
+					<strong>Teachers</strong>
+					<ul>
+						<li><strong>Annotated Teacher's Edition (print and digital)</strong>
+						Includes margin notes featuring games and activities, cultural information and teacher suggestions</li>
+						<li><strong>Teacher resources DVD</strong> includes the full Annotated Teacher's Edition plus all of the following program resources:
+							<ul>
+								<li>TEXTBOOK AUDIO PROGRAM includes all recordings from the textbook; dialogues, narratives, and activities. A Textbook Audio Program Manual provides the audio transcripts for the textbook audio.</li>
+								<li>LESSON PLANS include 50 minute and 90-minute program lesson plans.</li>
+								<li>WORKBOOK STUDENT'S EDITION includes activities to practice vocabulary, culture, and structure presented in the T'es branch&eacute;? program.</li>
+								<li>WORKBOOK TEACHER'S EDITION includes the answers to the student activities that reinforce vocabulary, grammar and cultural information.</li>
+								<li>DRILL AND PRACTICE GAMES include online games for students to reinforce vocabulary and grammar acquisition.</li>
+								<li>e-VISUALS are images and scenes to reinforce vocabulary. e-Visual instructions provide suggestions for use.</li>
+								<li>TEXTBOOK DIALOGUE VIDEOS with video episodes for each unit lesson</li>
+								<li>Rendez-vous &aacute; Nice! DVD PROGRAM includes video files of a continuous storyline video that reinforces each unit's vocabulary and grammar. A DVD Manual includes transcripts, student activity sheets, and an answer key.</li>
+								<li>LISTENING ACTIVITIES with MP3 Audio include student worksheets for listening comprehension activities as well as the audio and answer key.</li>
+								<li>ACTIVITIES FOR MASTERY offers games and activities to encourage students to practice the material presented in the T'es branch&eacute;? program.</li>
+								<li>COMMUNICATIVE ACTIVITIES Manual includes information gap activities, postcard activities, and situation card activities.
+								<li>COPY MASTERS include premade graphs and forms for students to fill out when completing certain textbook activities.</li>
+								<li>Lectures th&eacute;matiques MANUAL provides a variety of original and authentic readings for students as well as reading strategies and activities.</li>
+								<li>TPR STORYTELLING MANUAL includes Step-by-step instructions for basic and advanced stories. (Level 1 only)</li>
+								<li>Pre-AP LISTENING are audio files designed to help students practice comprehension skills in preparation for the AP exam.</li>
+								<li>FLASHCARDS are designed to help students learn vocabulary.</li>
+								<li>PRE-TESTS practice for students</li> 
+								<li>ASSESSMENT PROGRAM includes:
+									<ul>
+										<li>Lesson quizzes for each unit as well as the answer key</li>
+										<li>Unit tests with Answer Key and MP3 Audio to assess speaking, listening, reading and writing based on unit vocabulary, grammar, and culture.</li>
+										<li>Proficiency Tests</li>
+									</ul>
+								</li>
+								<li>ExamVIEW&reg; software is preloaded with the Student Pre-tests, and the Quizzes and Unit Tests from the Assessment Program that can be adapted or modified or printed and administered to students. Includes advanced, at-standard, and slower-paced listening comprehension MP3s.</li>
+							</ul>
+						</li>
+					</ul>
+				
+					<strong>Passport</strong>
+					<p><img src="/lib/images/georgia/Passport-Device-Agnostic-TEB.jpg" border="0" style="float:right;margin-left:10px;" />
+						Enhance teaching and increase student engagement with the learning environment designed for tomorrow's World Language classroom, Passport&reg;.<br /><br />
+						
+						Passport&copy; "brings it all home" with its unique anytime, anywhere learning platform. In the classroom, at home, on the go, students connect to curriculum, culture, 
+						classmates, and communities as well as showing what they know. Immerse your students in culture and language as well as capture and measure their progress with Passport's 
+						performance-based tasks and project-based learning.
+					</p>
+				</div>
+				<div>
+					<p>Passport&copy; features...
+					<ul>
+						<li>&iexcl;Qu&eacute; ch&eacute;vere! eBooks<br />
+						(textbook, workbook, listening activities,<br />
+						grammar and vocabulary exercises)</li>
+						<li>project-based learning</li>
+						<li>cultural immersion</li>
+						<li>video-based sharing</li>
+						<li>performance-based assessment</li>
+						<li>automatic grading and feedback</li>
+						<li>gradebook</li>
+						<li>class management</li>
+						<li>assigning</li>
+					</ul>
+				</div>
+				<div class="clearfix"></div>
+				<div>
+					<p>Immerse your students in culture and language as well as capture and measure their progress with Passport's performance-based tasks and project-based learning.</p>
+				</div>
+			</div>
+		</div>
+	<!-- DA -->
+		<div style="text-align:center;"><a name="da"></a><img src="/lib/images/georgia/GA-DA-header.jpg" border="0"></div>
+		<div>
+			<div class="ninehundred">
+				<h3>Deutsch Aktuell 6th edition</h3>
+				<strong>Program Resources</strong><br /><br />
+
+				<strong>Student</strong>
+				<ul>
+					<li>Textbook (print)</li>
+					<li>eBook  (digital)</li>
+					<li>Workbook (print)</li>
+					<li>eWorkbook (digital)</li>
+				</ul>
+
+				<strong>Teachers</strong><br />
+				Printed and digital resources:
+				<ul>
+					<li><strong>Annotated Teacher's Edition</strong><br />
+						Includes margin notes featuring games and activities, cultural information and teacher suggestions</li>
+					<li><strong>Workbook Teacher's Edition</strong> includes the answers to the student activities that reinforce vocabulary, grammar, and cultural information.</li>
+					<li><strong>eWorkbook Teacher's Edition</strong></li>
+					<li><strong>Communicative Activities</strong> includes information gap activities, postcard activities, and situation card activities.</li>
+					<li><strong>TPR Storytelling Manual</strong> includes Step-by-step instructions for basic and advanced stories. (Level 1 only)</li>
+				</ul>
+
+				Multimedia resources:
+				<ul>
+					<li><strong>Plan, Listening, and Assessment Pack with Audio CD</strong> includes: <i>Textbook Audio Program Manual; Textbook Audio Program CD; Listening Activities Manual with Answer Key</i> 
+						( additional listening comprehension activities); <i>Listening Activities Audio CD; Test Booklet</i> (Unit tests with audio and answer key); Quizzes (lesson quizzes with answer key); 
+						<i>Assessment Program Audio CDs; Lesson Planner &amp; ExamVIEW&reg; Assessment Suite DVD</i> (Lesson and Unit Tests that can be adapted or modified or printed and administered to students)
+					<li><strong>Electronic Overhead Transparencies CD</strong> includes images and scenes to reinforce vocabulary. e-Visual instructions provide suggestions for use.
+					<li><strong>Treffpunkt Berlin DVD Program</strong> with video files of a continuous storyline that reinforces each unit's vocabulary and grammar. Includes DVD manual with transcripts, student activity sheets, and an answer key.
+
+				</ul>
+				
+				<strong>Passport</strong>
+				<p>Enhance teaching and increase student engagement with the learning environment designed for tomorrow's World Language classroom, Passport&reg;.</p>
+				<p>Passport&copy; "brings it all home" with its unique anytime, anywhere learning platform. In the classroom, at home, on the go, students connect to curriculum, culture, 
+				classmates, and communities as well as showing what they know. Immerse your students in culture and language as well as capture and measure their progress with Passport's 
+				performance-based tasks and project-based learning.</p>
+
+				<p>Passport&copy; features...
+				<ul>
+					<li>&iexcl;Qu&eacute; ch&eacute;vere! eBooks (textbook, workbook, listening activities, grammar and vocabulary exercises)</li>
+					<li>project-based learning</li>
+					<li>cultural immersion</li>
+					<li>video-based sharing</li>
+					<li>performance-based assessment</li>
+					<li>automatic grading and feedback</li>
+					<li>gradebook</li>
+					<li>class management</li>
+					<li>assigning</li>
+				</ul>
+				<p>Immerse your students in culture and language as well as capture and measure their progress with Passport's performance-based tasks and project-based learning.</p>
+			</div>
+		</div>
+	<!-- Zhen Bang -->
+		<div style="text-align:center;"><a name="zb"></a><img src="/lib/images/georgia/GA-ZB-header.jpg" border="0"></div>
+		<div>
+			<div class="ninehundred">
+				<h3>Zhen B&agrave;ng</h3>
+				<strong>Program Resources</strong><br /><br />
+
+				<strong>Student</strong>
+				<ul>
+					<li>Textbook</li>
+					<li>Workbook</li>
+					<li>Characters Practice Book</li>
+					<li>ebook</li>
+					<li>Listening Activities with MP3 Audio includes student worksheets for listening comprehension activities as well as the audio and answer key.</li>
+					<li>Online Flash game</li>
+					<li>Textbook Audio program includes all recordings from the textbook; dialogues, narratives, and activities.</li>
+					<li>Character Practice Book Audio MP3s</li>
+					<li>Video Program: ZhenBang has a serious storyline videos that reinforce each lesson's vocabulary and grammar</li>
+					<li>Culture Videos</li> 
+					<li>Pronunciation Videos</li>
+					<li>Story of Characters Animations</li>
+					<li>Unit Review Lessons </li>
+				</ul>
+
+				<strong>Teachers</strong>
+				<ul>
+					<li>Annotated Teacher's Edition includes margin notes featuring games and activities, cultural information and teacher suggestions</li>
+					<li>Workbook Answer Key</li>
+					<li>Character Practice Answer Key</li>
+					<li>Assessment Answer Key</li> 
+					<li>Assessments (24 lesson quizzes, 6 unit tests, 1 midterm and 1 final)</li>
+				</ul>
+				
+				<p>Note: Teacher has the access to all student resources listed above</p>
+				
+				<strong>Passport (Chinese coming soon)</strong>
+				<p>Enhance teaching and increase student engagement with the learning environment designed for tomorrow's World Language classroom, Passport&reg;.</p>
+				<p>Passport&copy; "brings it all home" with its unique anytime, anywhere learning platform. In the classroom, at home, on the go, students connect to curriculum, culture, 
+				classmates, and communities as well as showing what they know. Immerse your students in culture and language as well as capture and measure their progress with Passport's 
+				performance-based tasks and project-based learning.</p>
+
+				<p>Passport&copy; features...
+				<ul>
+					<li>ZhenBang! eBooks (textbook, workbook, listening activities, grammar and vocabulary exercises)</li>
+					<li>project-based learning</li>
+					<li>cultural immersion</li>
+					<li>video-based sharing</li>
+					<li>performance-based assessment</li>
+					<li>automatic grading and feedback</li>
+					<li>gradebook</li>
+					<li>class management</li>
+					<li>assigning</li>
+				</ul>
+				<p>Immerse your students in culture and language as well as capture and measure their progress with Passport's performance-based tasks and project-based learning.</p>
+			</div>
+		</div>
+	</div>
+<map name="header">
+<area shape="rect" coords="495,413,603,560" href="#qc" alt="">
+<area shape="rect" coords="642,413,751,560" href="#teb" alt="">
+<area shape="rect" coords="791,413,900,560" href="#da" alt="">
+<area shape="rect" coords="937,413,1046,560" href="#zb" alt=""> 
+</map>
+<?php include 'includes/footer.php' ?>
