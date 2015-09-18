cab65b948d249b55dbe6632f9fdd7b9f4f8de27b
Brad Knutson <brad.s.knutson@gmail.com>
2015-09-18 14:04:18 -0500

    World Language Readers Page Initial Commit

---
+++ world-languages/readers/index.php
@@ -0,0 +1,48 @@
+ <?php 
+	$custom_title = 'World Language Readers | ';
+	include '../../includes/header.php';
+?>
+	<div id="container" class="wl-readers">
+		<div class="vimeo-modal"></div>
+		<div class="header-background-image wlr-bg">
+			<div id="topImg"></div>
+			<div class="colorBar" style="background:#c10006; height:10px; width:100%; margin-top:-37px; position:relative; top:35px;"></div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="secondary-first">
+			<div class="ninesixty">
+				<div class="conectate-left margintop40">
+					<p class="no-top">Enhanced eReaders connect students to their books in and out of the classroom. eReaders work on all laptops, desktops, and mobile devices including the iPad<sup>&copy;</sup>. eReaders are available in Spanish, French and German.</p>
+                    
+                    <p>Features:</p>
+                    
+                    <ul>
+                        <li>Beautiful illustrations</li>
+                        <li>Synchronized audio</li>
+                        <li>Note taking</li>
+                        <li>Interactive activities</li>
+                        <li>Games</li>
+                    </ul>
+                    
+                    <p><a class="learn-more-link smaller-link" href="<?php echo $server; ?>world-languages/readers/buy/">Buy Now!</a></p>
+                    <p><a class="learn-more-link smaller-link" href="<?php echo $server; ?>ereaders/">Learn more!</a></p>
+				</div>
+				<div class="conectate-right">
+					<img src="<?php echo $server; ?>lib/images/cideb.png" alt="CIDEB Readers" />
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+        
+        <div class="redefining no-bottom">
+            <div class="ninesixty paddingtop40 paddingbottom40">
+                <h2>EMC Paperback Readers</h2>
+                <p>Prefer the no-power-required type of reader? You’re at the right place. Either as stand-alone books or as extensions to our textbook series, readers develop your learners' grammar and vocabulary while building their ability to comprehend increasingly difficult pieces of literature.</p>
+                
+                <p><a class="learn-more-link smaller-link" href="<?php echo $server; ?>world-languages/readers/paperback/buy/">Buy Now!</a></p>
+            </div>
+        </div>
+
+<?php include '../../includes/footer.php'; ?>
