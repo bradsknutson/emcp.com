76fab056ef69364bf2bec8050ef343289aca02e1
weinerdog <bmolina@emcp.com>
2015-10-12 08:42:52 -0500

    Catalog page edits

---
+++ catalog/index.php
@@ -0,0 +1,37 @@
+ <?php 
+	$custom_title = '2016 Catalog';
+	include '../includes/header.php';
+?>
+	<div id="container">
+		<div class="header-background-image cat-bg">
+			<div id="topImg"></div>
+		</div>
+		<div class="header-top">
+			<?php include '../includes/nav.php'; ?>
+		</div>
+		<div class="white-slice al-top-content">
+			<div class="ninesixty">
+                <div>
+					<p style="margin-top:0;">Thanks for your interest in EMC’s 2015 catalogs. Scroll down to view, download or share our English Language Arts, Social Studies, and World Languages catalogs.</p>
+					<p>Questions? Please feel free to contact your <a href="http://store.emcp.com/about-emcp/local-account-manager-locator">Account Manager</a>.</p>
+				</div>
+                <div class="clearfix"></div>
+            </div>
+			<div style="margin-top:17px;">
+				<iframe name="Joomag_embed_70589099-3f72-4a2a-a06e-88324c454398" style="width:100%;height:800px;" width="100%" height="800" hspace="0" vspace="0" frameborder="0" 
+				src="https://www.joomag.com/magazine/emc-world-language-catalog-2016/0303065001442595431?p=1&amp;e=1&amp;embedInfo=;image,%2F%2Fwww.joomag.com%2Fstatic%2Fflash%2Fgui%2Fthemes%2Fdefault%2Fbg.jpg,fill">
+				</iframe>
+			</div>
+			<div style="margin-top:17px;">
+				<iframe name="Joomag_embed_d2bb34b7-6f52-43af-954f-fb52236b46c3" style="width:100%;height:800px;" width="100%" height="800" hspace="0" vspace="0" frameborder="0" 
+				src="https://www.joomag.com/magazine/emc-english-language-art-catalog/0303253001442598254?p=1&amp;e=1&amp;embedInfo=;image,%2F%2Fwww.joomag.com%2Fstatic%2Fflash%2Fgui%2Fthemes%2Fdefault%2Fbg.jpg,fill">
+				</iframe>
+			<div style="margin-top:17px;">
+				<iframe name="Joomag_embed_8120f70c-2aa2-48fc-84a1-390fd1fc0ffd" style="width:100%;height:800px;" width="100%" height="800" hspace="0" vspace="0" frameborder="0" 
+				src="https://www.joomag.com/magazine/emc-applied-learning-catalog-2016/0123595001442598068?p=1&e=1&embedInfo=;image,%2F%2Fwww.joomag.com%2Fstatic%2Fflash%2Fgui%2Fthemes%2Fdefault%2Fbg.jpg,fill">
+				</iframe>
+			</div>
+        </div>
+    </div>
+
+<?php include '../includes/footer.php'; ?>
