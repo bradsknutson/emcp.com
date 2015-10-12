2e9f0bee111311ef674c2c7ca9952f4188301cf5
Brad Knutson <brad.s.knutson@gmail.com>
2015-10-12 16:17:58 -0500

    Catalog Page Updates

--- catalog/index.php
+++ catalog/index.php
@@ -9,7 +9,7 @@
 		<div class="header-top">
 			<?php include '../includes/nav.php'; ?>
 		</div>
-		<div class="white-slice al-top-content">
+		<div class="white-slice light-grey-slice al-top-content">
 			<div class="ninesixty">
                 <div>
 					<p style="margin-top:0;">Thanks for your interest in EMC’s 2015 catalogs. Scroll down to view, download or share our English Language Arts, Social Studies, and World Languages catalogs.</p>
@@ -17,20 +17,26 @@
 				</div>
                 <div class="clearfix"></div>
             </div>
-			<div style="margin-top:17px;">
-				<iframe name="Joomag_embed_70589099-3f72-4a2a-a06e-88324c454398" style="width:100%;height:800px;" width="100%" height="800" hspace="0" vspace="0" frameborder="0" 
-				src="https://www.joomag.com/magazine/emc-world-language-catalog-2016/0303065001442595431?p=1&amp;e=1&amp;embedInfo=;image,%2F%2Fwww.joomag.com%2Fstatic%2Fflash%2Fgui%2Fthemes%2Fdefault%2Fbg.jpg,fill">
-				</iframe>
-			</div>
-			<div style="margin-top:17px;">
-				<iframe name="Joomag_embed_d2bb34b7-6f52-43af-954f-fb52236b46c3" style="width:100%;height:800px;" width="100%" height="800" hspace="0" vspace="0" frameborder="0" 
-				src="https://www.joomag.com/magazine/emc-english-language-art-catalog/0303253001442598254?p=1&amp;e=1&amp;embedInfo=;image,%2F%2Fwww.joomag.com%2Fstatic%2Fflash%2Fgui%2Fthemes%2Fdefault%2Fbg.jpg,fill">
-				</iframe>
-			<div style="margin-top:17px;">
-				<iframe name="Joomag_embed_8120f70c-2aa2-48fc-84a1-390fd1fc0ffd" style="width:100%;height:800px;" width="100%" height="800" hspace="0" vspace="0" frameborder="0" 
-				src="https://www.joomag.com/magazine/emc-applied-learning-catalog-2016/0123595001442598068?p=1&e=1&embedInfo=;image,%2F%2Fwww.joomag.com%2Fstatic%2Fflash%2Fgui%2Fthemes%2Fdefault%2Fbg.jpg,fill">
-				</iframe>
-			</div>
+        </div>
+        <div class="white-slice">
+			<div class="ninesixty">
+                <div class="catalog-img">
+                    <a href="http://joom.ag/vbNp" target="_blank">
+                        <img src="<?php echo $server; ?>lib/images/catalog-wl.png" alt="World Languages Catalog" class="animated" />
+                    </a>
+                </div>
+                <div class="catalog-img">
+                    <a href="http://joom.ag/WpNp" target="_blank">
+                        <img src="<?php echo $server; ?>lib/images/catalog-ela.png" alt="English Language Arts Catalog" class="animated" />
+                    </a>
+                </div>
+                <div class="catalog-img">
+                    <a href="http://joom.ag/YpNp" target="_blank">
+                        <img src="<?php echo $server; ?>lib/images/catalog-al.png" alt="Applied Learning Catalog" class="animated" />
+                    </a>
+                </div>
+                <div class="clearfix"></div>
+            </div>            
         </div>
     </div>
 
