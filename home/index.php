aabff45d29df6e674c947600db66433516724718
Brad Knutson <brad.s.knutson@gmail.com>
2015-12-16 14:34:30 -0600

    Home Page V2 Updates

--- home/index.php
+++ home/index.php
@@ -29,15 +29,19 @@
 				<div class="clearfix"></div>
 			</div>
 		</div>
-        <div class="emc-connect">
-            <div class="ninehundred">
+        <div class="connect-slice">
+            <div class="ninesixty">
                 <div class="emc-connect-logo">
-                    <img src="<?php echo $server; ?>lib/images/emc-connect-logo.png" alt="EMC Connect" />
+                    <img src="<?php echo $server; ?>lib/images/emc-connect-black.png" alt="EMC Connect" />
                 </div>
                 <div class="emc-connect-content">
-                    <?php include '../includes/home.php'; ?>
+                    <p>Visit EMC Connect for personalized teaching tips, free classroom resources and trending education articles from around the web. You can also catch up on the latest news from EMC!</p>
                 </div>
                 <div class="clearfix"></div>
+                <div class="emc-connect-posts">
+                    <?php include '../includes/home.php'; ?>
+                    <div class="clearfix"></div>
+                </div>
             </div>
         </div>
 		<div class="emc-technology-bg"></div>
@@ -53,15 +57,19 @@
 						</div>
 					</div>
 					<div class="ell-right">
-                        <p>Through a long-term collaborative partnership with the University of Minnesota's Institute for Design Innovation (IDI) we are building, integrating, and researching the future of fluency. <span class="bold">Passport&reg;</span> builds upon and extends research-based language learning platforms created by the IDI, leveraging EMC Publishing's world language instructional content in an easy-to-use environment designed to drive educator effectiveness and student fluency. Students will <span class="bold">discover</span> languages and cultures, <span class="bold">expand</span> their knowledge beyond the textbook and written test, and <span class="bold">perform</span> what they’ve learned within a portfolio-style environment to achieve lasting proficiency.</p>
+                        <p>Through a long-term collaborative partnership with the University of Minnesota's Institute for Design Innovation (IDI) we are building, integrating, and researching the future of fluency. <span class="bold">Passport&reg;</span> builds upon and extends research-based language learning platforms created by the IDI, leveraging EMC School's world language instructional content in an easy-to-use environment designed to drive educator effectiveness and student fluency. Students will <span class="bold">discover</span> languages and cultures, <span class="bold">expand</span> their knowledge beyond the textbook and written test, and <span class="bold">perform</span> what they’ve learned within a portfolio-style environment to achieve lasting proficiency.</p>
 						
 						<a href="<?php echo $server; ?>passport" class="learn-more-link">Learn more about Passport&reg;</a>
 					</div>
 					<div class="clearfix"></div>
 				</div>
-				<h2>Explore our award-winning library in the new EMC store.</h2>
 			</div>
 		</div>
+        <div class="light-grey-slice redefining paddingtop40 paddingbottom80">
+            <div class="ninesixty">
+                <h2>Explore our award-winning library in the new EMC store.</h2>
+            </div>
+        </div>
 		<div class="emc-store">
 		<div class="emc-store-bg"></div>
 			<div class="seveneighty">				
@@ -73,44 +81,4 @@
 				</p>
 			</div>
 		</div>
-		<div class="redefining">
-			<div class="ninehundred no-bottom">
-				<div class="aligncenter">
-					<h2>Redefining seamless learning through technology.</h2>
-				</div>
-				
-				<p>Through our partnership with the University of Minnesota's Institute for Design Innovation (IDI), we are now offering award-winning, research-based learning platforms that enhance your face-to-face, blended, or online classroom.</p>
-			</div>
-			<div class="redefining-bg">
-				<div class="vimeo-modals-container">
-					<div class="avenue-play-button">
-						<div class="play-button">
-							<div class="icon-play"></div>
-						</div>
-					</div>
-					<div class="avenue-play-button-hover-background"></div>
-					<div class="flipgrid-play-button">
-						<div class="play-button">
-							<div class="icon-play"></div>
-						</div>
-					</div>
-					<div class="flipgrid-play-button-hover-background"></div>
-				</div>
-			</div>
-			<div class="ninesixty">
-				<div class="avenue-flipgrid">
-					<div class="one-half first">
-						<p><span class="bold">Avenue&trade;</span> is a simple, yet powerful e-assessment platform where your students build portfolios of their spoken performances. Educators can create custom tasks or build tasks based on EMC's rich, pre-loaded library of media.</p>
-						
-						<a href="<?php echo $server; ?>technology/avenue/" target="_blank" class="learn-more-link">Learn more about Avenue</a>
-					</div>
-					<div class="one-half last">
-						<p><span class="bold">Flipgrid&trade;</span> is a video-based discussion environment where students respond to short, teacher-created questions. Flipgrid brings the back row to the front in your classroom.</p>
-						
-						<a href="<?php echo $server; ?>technology/flipgrid/" target="_blank" class="learn-more-link">Learn more about Flipgrid</a>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>=
 <?php include '../includes/footer.php' ?>
