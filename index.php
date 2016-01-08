2f46b7f0a8e3cc27964b657edd469031570c009f
Brad Knutson <brad.s.knutson@gmail.com>
2016-01-08 15:40:17 -0600

    Home Page push

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include 'includes/header.php'; ?>
-	<div id="container" class="main">
+	<div id="container" class="main home home-new">
 		<div class="header-background-image">
 			<div id="topImg">
 				<div class="play-button">
@@ -7,7 +7,8 @@
 				</div>
 				<div class="header-hero-hover"></div>
 				<div class="future-fluency">
-					<h2>The future of fluency</h2>
+					<h2>Meet Passport<sup>&reg;</sup><br />
+                        The future of fluency</h2>
 				</div>				
 			</div>
 		</div>
@@ -15,137 +16,39 @@
 		<div class="header-top">
 			<?php include 'includes/nav.php'; ?>
 		</div>
-		<div class="home-first">
-			<div class="ninehundred">
-				<span class="large-red">For nearly 60 years</span>, EMC Publishing has been developing and producing innovative and award-winning K-12 learning solutions that seamlessly connects learners and teachers in World Languages, Language Arts, and Applied Learning. We're just getting started.
-			</div>
-		</div>
-		<div class="students-bg"></div>
-		<div class="grey-bar">
-			<div class="ninehundred" style="padding: 17px 0px;">Explore EMC's three core domains of teaching and learning</div>
-		</div>
-		<div class="emc-cats">
-			<div class="ninehundred cleafix">
-				<div class="one-third first">
-					<h3>World Languages</h3>
-					
-					<p>Our World Languages line includes robust textbook programs and cutting-edge digital resources to enhance any K-12+ classroom with video-based tools, authentic cultural videos, eBooks, and much more.</p>
-					
-					<a href="<?php echo $server; ?>world-languages/" class="learn-more-link">Learn more</a>
+		<div class="home-first emc-cats">
+			<div class="ninesixty paddingtop40 paddingbottom40 cleafix">
+				<div class="one-fourth first">
+                    <a href="<?php echo $server; ?>world-languages/" class="learn-more-link"><h3>World Languages</h3></a>
 				</div>
-				<div class="one-third">
-					<h3>Language Arts</h3>
-					
-					<p>Our Literature and Language Arts materials have been developed with a devotion to rigor, innovation, teacher utility, and customer service that will transform language arts teaching and learning.</p>
-					
-					<a href="http://store.emcp.com/english-language-arts.html" class="learn-more-link">Learn more</a>
+				<div class="one-fourth second">
+                    <a href="<?php echo $server; ?>language-arts/" class="learn-more-link"><h3>Language Arts</h3></a>
 				</div>
-				<div class="one-third last">
-					<h3>Applied Learning</h3>
-					
-					<p>Empower your students with real-world applications of anatomy, physiology, and biotechnology concepts, as well as innovative 21st century career and life skills learning solutions.</p>
-					
-					<a href="" class="learn-more-link">Learn more</a>
+				<div class="one-fourth third">
+                    <a href="<?php echo $server; ?>applied-learning/" class="learn-more-link"><h3>Applied Learning</h3></a>
+				</div>
+				<div class="one-fourth last">
+                    <a href="http://store.emcp.com" class="learn-more-link"><h3>Store</h3></a>
 				</div>
 				<div class="clearfix"></div>
 			</div>
 		</div>
-		<div class="emc-revolutionized">
-			<div class="ninehundred">
-				<p><span class="sixty-a">60 years ago</span></p>
-				<p style="padding-top: 10px;"><span class="sixty-b">EMC revolutionized technology in the world language classroom.</span></p>
-				<p><span class="sixty-c">Now, we are doing it again.</span></p>
-			</div>
-		</div>
-		<div class="emc-technology-bg"></div>
-		<div class="emc-languages">
-			<div class="ninehundred no-top">
-				<div class="emc-languages-learn">
-					<div class="ell-left">
-						<div class="quote">
-							<span class="red-quotes">"</span>The future of fluency depends on building student competency through engaging performance.<span class="red-quotes">"</span>
-						</div>
-						<div class="quote-author">
-							<strong>- Eric Cantor</strong>, CEO<br />New Mountain Learning
-						</div>
-					</div>
-					<div class="ell-right">
-                        <p>Through a long-term collaborative partnership with the University of Minnesota's Institute for Design Innovation (IDI) we are building, integrating, and researching the future of fluency. <span class="bold">Passport&reg;</span> builds upon and extends research-based language learning platforms created by the IDI, leveraging EMC Publishing's world language instructional content in an easy-to-use environment designed to drive educator effectiveness and student fluency. Students will <span class="bold">discover</span> languages and cultures, <span class="bold">expand</span> their knowledge beyond the textbook and written test, and <span class="bold">perform</span> what they’ve learned within a portfolio-style environment to achieve lasting proficiency.</p>
-						
-						<a href="<?php echo $server; ?>passport" class="learn-more-link">Learn more about Passport&reg;</a>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-				<h2>Explore our award-winning library in the new EMC store.</h2>
-			</div>
-		</div>
-		<div class="emc-store">
-		<div class="emc-store-bg"></div>
-			<div class="seveneighty">				
-				<div class="aligncenter">
-					<a href="http://store.emcp.com" class="learn-more-link larger-link">Explore the EMC store</a>
-				</div>
-				<p>
-					Our newly designed EMC store is your hub for all of our innovative and award-winning products and supplemental materials. You'll find our World Languages, Applied Learning, and Language Arts curriculum series, in addition to our multiplatform eBooks and research-based digital learning environments Avenue&trade;, Flipgrid&trade;, and i-Culture&trade;.
-				</p>
-			</div>
-		</div>
-		<div class="redefining">
-			<div class="ninehundred no-bottom">
-				<div class="aligncenter">
-					<h2>Redefining seamless learning through technology.</h2>
-				</div>
-				
-				<p>Through our partnership with the University of Minnesota's Institute for Design Innovation (IDI), we are now offering award-winning, research-based learning platforms that enhance your face-to-face, blended, or online classroom.</p>
-			</div>
-			<div class="redefining-bg">
-				<div class="vimeo-modals-container">
-					<div class="avenue-play-button">
-						<div class="play-button">
-							<div class="icon-play"></div>
-						</div>
-					</div>
-					<div class="avenue-play-button-hover-background"></div>
-					<div class="flipgrid-play-button">
-						<div class="play-button">
-							<div class="icon-play"></div>
-						</div>
-					</div>
-					<div class="flipgrid-play-button-hover-background"></div>
-				</div>
-			</div>
-			<div class="ninesixty">
-				<div class="avenue-flipgrid">
-					<div class="one-half first">
-						<p><span class="bold">Avenue&trade;</span> is a simple, yet powerful e-assessment platform where your students build portfolios of their spoken performances. Educators can create custom tasks or build tasks based on EMC's rich, pre-loaded library of media.</p>
-						
-						<a href="<?php echo $server; ?>technology/avenue/" target="_blank" class="learn-more-link">Learn more about Avenue</a>
-					</div>
-					<div class="one-half last">
-						<p><span class="bold">Flipgrid&trade;</span> is a video-based discussion environment where students respond to short, teacher-created questions. Flipgrid brings the back row to the front in your classroom.</p>
-						
-						<a href="<?php echo $server; ?>technology/flipgrid/" target="_blank" class="learn-more-link">Learn more about Flipgrid</a>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-        <!-- <div id="sniplyBar" class="animated social bottom desktop">
-            <span id="socialbar" class="social bottom desktop">
-                <a href="http://paradigm.emcp.com" target="_blank">
-                    <img id="profilePhoto" class="sized social bottom desktop" src="<?php echo $server; ?>lib/images/pes-logo.jpg" alt="Paradigm Education Solutions" />
-                </a>
-            </span>
-            <span class="messageContainer sized social bottom desktop" id="contentMessageContainer">
-                <p id="minimizeToggle" pclass="social bottom desktop">x</p>
-                <a id="profileName" href="http://paradigm.emcp.com" target="_blank" class="social bottom desktop">Paradigm Education Solutions</a>
-                <span id="message" onclick="window.open('http://paradigm.emcp.com', '_blank'); " class="social bottom desktop">
-                    <span id="messageText" class="social bottom desktop">Looking for Paradigm Education Solutions?</span>
-                    <a href="http://paradigm.emcp.com" target="_blank" class="button buttonAction sized social bottom desktop">Click Here!</a>
-                </span>
-            </span>
-            <span class="messageContainer social bottom desktop" id="dropshadowMessageContainer">
-                <span id="speechArrow" class="social bottom desktop"></span>
-            </span>
-        </div> --> 
-<?php include 'includes/footer.php' ?>
+        <div class="about-emc">
+            <div class="ninehundred">
+                <h2 class="home-mission-h2">About EMC</h2>
+                <p class="home-mission">EMC is revolutionizing the way students are taught and learn, both in and out of the classroom. Through its innovative learning environment, Passport, EMC delivers digital learning solutions, marrying interactive curriculum with unique video based collaboration and assessment tools to promote student engagement, proficiency and culture in World Languages and English Language Arts. EMC &mdash; improving how teachers teach and students learn.</p>
+            </div>
+        </div>
+        <div class="connect-slice">
+            <div class="ninesixty">
+                <div class="emc-connect-logo emc-connect-content">
+                    <a href="http://connect.emcp.com" target="_blank"><img src="<?php echo $server; ?>lib/images/emc-connect-black.png" alt="EMC Connect" /></a>
+                    <p class="emc-connect-content">Tips, resources, trends, and more!</p>
+                </div>
+                <div class="emc-connect-posts">
+                    <?php include 'includes/home.php'; ?>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+<?php include 'includes/footer-new.php' ?>
