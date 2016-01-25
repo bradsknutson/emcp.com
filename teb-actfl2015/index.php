1851ee6f742395b1f3201860b3db47e2af2c870b
Brad Knutson <brad.s.knutson@gmail.com>
2016-01-25 09:32:11 -0600

    TEB-ACTFL2015 Page

---
+++ teb-actfl2015/index.php
@@ -0,0 +1,22 @@
+<?php 
+	include '../includes/header.php';
+?>
+	<div id="container">
+		<div class="header-secondary-page"></div>
+		<div class="header-top-dark">
+			<?php include '../includes/nav.php'; ?>
+		</div>
+        <div class="page-first white onesix about-first">
+            <div class="ninesixty paddingtop40 paddingbottom40">
+                <h2>Thank you for coming to our session at ACTFL</h2>
+                <p>Below you will find the presentation and corresponding worksheets as originally presented by Diana Moen, EMC Publishing's Senior Editor, in San Diego for ACTFL 2015.</p>
+                
+                <p style="margin-bottom: 40px;"><a class="technology-buttons teb-acftl2015-1 transition" href="http://www.emcp.com/lib/assets/EMC_ACTFL2015_final_NG.pptx" target="_blank">Targeting 90%: Immersion Lessons In Art and Literature</a></p>
+                <p><a class="technology-buttons teb-acftl2015-2 transition" href="http://www.emcp.com/lib/assets/Worksheets.pptx" target="_blank">Worksheets</a></p>
+                
+            </div>
+        </div>    
+    </div>
+	
+	
+<?php include '../includes/footer.php'; ?>
