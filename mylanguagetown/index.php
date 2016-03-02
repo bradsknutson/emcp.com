570fdb39a04f9cf5a27db4cf468f0dceb9283a96
weinerdog <bmolina@emcp.com>
2016-03-02 11:01:09 -0600

    MyLanguageTown page edits

--- mylanguagetown/index.php
+++ mylanguagetown/index.php
@@ -36,8 +36,8 @@
 		<div class="secondary-first">
 			<div class="ninesixty">
                 <img src="<?php echo $server; ?>lib/images/mlt-ereader.png" alt="My Language Town eReaders" style="float:right;margin-top:100px;"/>
+				<p style="margin:15px 0 -10px;"><a class="technology-buttons transition" style="display:inline !important;position:relative !important;bottom:0px !important;" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00eafa7e88f41d8cbd96a0269d731b013d8d034e9a8d6f0718f7" target="_blank" onClick="ga('send', 'event', 'Call To Action Button','click','MLT Sample');">Preorder your free sample today!</a></p>
                 <h1 class="no-bottom">Available June 1st starting at $2.95</h1>
-				<p><a class="technology-buttons mw-sample transition" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00eafa7e88f41d8cbd96a0269d731b013d8d034e9a8d6f0718f7" "target="_blank" onClick="ga('send', 'event', 'Call To Action Button','click','MLT Sample');">Preorder your free sample today!</a></p>
                 <p class="no-top">We all know how important reading, listening and having fun are for learning and perfecting your native language. It is equally important when learning a second language. My Language Town gives you this opportunity and provides you a variety of titles for each level that can be used with any program.</p>
                 
                 <p>Designed for use both in the classroom and as an independent study, My Language Town eBooks can make learning a new language an interactive experience. My Language Town eBooks can be read aloud in class, in a small group, at home with the family and friends, as a play with a study group, or even alone at your kitchen table.</p>
@@ -50,6 +50,21 @@
                 
                 <div id="accordion">
 			<!-- 1 -->
+					<h3 class="showSingle" target="2"><strong>Beginner Level eReaders </strong></h3>
+					<div id="div2" class="targetDiv" style="display:none;">
+                        <p>Embark on Christopher Columbus's journey to the New World or join the Rodríguez in their hilarious quest for a legendary Mayan crown amongst Chihuahuas and mariachis. My Language Town Beginners Series is packed with thrills, laughs and adventure. My Language Town is the hometown for every student of Spanish language. Come join us!</p>
+					</div>
+			<!-- 2 -->
+					<h3 class="showSingle" target="3"><strong>Elementary Level eReaders </strong></h3>
+					<div id="div3" class="targetDiv" style="display:none;">
+                        <p>My Language Town's Elementary Spanish Series is a fantastic collection of quick reads to learn elementary Spanish effortlessly&mdash;including songs, stories, poems and plenty of delightful illustrations. Students learn everyday vocabulary, basic sentences and expressions in a highly interactive, engaging and effective way.</p>
+					</div>
+			<!-- 3 -->
+					<h3 class="showSingle" target="5"><strong>Low intermediate/Intermediate Level eReaders</strong></h3>
+					<div id="div5" class="targetDiv" style="display:none;">
+                        <p>My Language Town's Intermediate Series is the place to drop by for those students of Spanish in the mood for adventure. Fact or fiction, historical or contemporary, quick reads or longer novels, a war romance in Madrid or a tango-paced murder mystery in Buenos Aires&mdash;you get to choose!</p>
+					</div>
+			<!-- 4 -->		
 					<h3 class="showSingle" target="1"><strong>How to use My Language Town eBooks</strong></h3>
 					<div id="div1" class="targetDiv" style="display:none;">
 						<p>The most Diverse Range of Literature and Informational Texts</p>
@@ -63,22 +78,6 @@
 							<li>Students can research and present (written or oral) cultural facts that appear in each book and more!</li>
 						</ul>
 					</div>
-			<!-- 2 -->
-					<h3 class="showSingle" target="2"><strong>Beginner Level eReaders </strong></h3>
-					<div id="div2" class="targetDiv" style="display:none;">
-                        <p>Embark on Christopher Columbus's journey to the New World or join the Rodríguez in their hilarious quest for a legendary Mayan crown amongst Chihuahuas and mariachis. My Language Town Beginners Series is packed with thrills, laughs and adventure. My Language Town is the hometown for every student of Spanish language. Come join us!</p>
-					</div>
-			<!-- 3 -->
-					<h3 class="showSingle" target="3"><strong>Elementary Level eReaders </strong></h3>
-					<div id="div3" class="targetDiv" style="display:none;">
-                        <p>My Language Town's Elementary Spanish Series is a fantastic collection of quick reads to learn elementary Spanish effortlessly&mdash;including songs, stories, poems and plenty of delightful illustrations. Students learn everyday vocabulary, basic sentences and expressions in a highly interactive, engaging and effective way.</p>
-					</div>
-			<!-- 4 -->
-					<h3 class="showSingle" target="5"><strong>Low intermediate/Intermediate Level eReaders</strong></h3>
-					<div id="div5" class="targetDiv" style="display:none;">
-                        <p>My Language Town's Intermediate Series is the place to drop by for those students of Spanish in the mood for adventure. Fact or fiction, historical or contemporary, quick reads or longer novels, a war romance in Madrid or a tango-paced murder mystery in Buenos Aires&mdash;you get to choose!
-</p>
-					</div>
 				</div>
                 
             </div>
@@ -87,7 +86,7 @@
         <div class="redefining">
             <div class="ninesixty paddingtop40 paddingbottom40">
                 
-                <h2 class="no-bottom">The power of teaching Spanish through songs</h2>
+                <h2 class="no-bottom" style="margin-bottom:13px !important;">The power of teaching Spanish through songs</h2>
                 <h3 class="no-top">The results are simply incredible. The possibilities are endless.</h3>
                 <p>Move, shake and learn to the beat! These catchy, popular songs are created to help students learn Spanish in a fun and easy way. Songs are available for Elementary, Beginner and Low-intermediate/Intermediate level students.
 
