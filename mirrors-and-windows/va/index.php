36fa898a41c9c6ab20fd7dae6f28191e4eddf38c
Brad Knutson <brad.s.knutson@gmail.com>
2016-03-18 13:20:56 -0500

    ELA VA Adoption Pages

---
+++ mirrors-and-windows/va/index.php
@@ -0,0 +1,248 @@
+
+<?php 
+	$custom_title = 'Virginia | Mirrors & Windows | ';
+	include '../../includes/header.php';
+?>
+<style>
+.ui-accordion .ui-accordion-header {
+	background: #F0EFEB !important;
+	font-size: 22px !important;
+	padding-right: .2em !important;
+}
+.redefining p {
+	font-size: 20px !important;
+}
+.ui-helper-reset {
+	font-size: 18px !important;
+}
+.ui-widget {
+	font-family: "myriad-pro", sans-serif !important;
+}
+.ui-accordion-content {
+    height: 300px !important;
+}
+</style>
+	<div id="container" class="mw">
+		<div class="header-background-image mw-va-bg">
+			<div id="topImg">
+			</div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav-ca.php'; ?>
+		</div>
+		<div class="secondary-first">
+			<div class="ninesixty">
+				<div>
+                    <p class="no-top"><span class="bold">EMC Publishing</span> is proud to present its exciting literature program, <span class="italic">Mirrors &amp; Windows</span>. 
+					This <span class="bold">seven-level program (grades 6-12)</span> is built on a collection of rich, diverse, and timeless writings by renowned, award-winning authors.</p>
+					
+					<a class="mw-buttons" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00ea1213cf5f7e1fd4238c11d2d35467b46e2d6f930af6656a96" target="_blank">Sample</a>
+					&nbsp;&nbsp;
+					<a class="mw-buttons" href="https://vimeo.com/138627634" target="_blank">Watch Video</a>
+					&nbsp;&nbsp;
+					<a class="mw-buttons" href="resources/" target="_blank">Resources</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	<!-- NEW -->
+		<div style="padding:10px 0;margin:0;background:#9E0C10;">
+			<h2 style="font-size:34px;margin:15px 0 25px;text-align:center;color:#fff;">Top Reasons Mirrors & Windows is Perfect for Virginia Teachers</h2>
+		</div>
+        <div class="redefining no-bottom">
+            <div class="ninesixty paddingbottom40" style="margin-top:15px;">
+				<div id="accordion">
+			<!-- 1 -->
+					<h3 class="showSingle" target="1"><strong>1. Love of Literature</strong></h2>
+					<div id="div1" class="targetDiv" style="display:none;">
+						<p>The most Diverse Range of Literature and Informational Texts</p>
+						<ul>
+							<li>The most Diverse Range of Literature and Informational Texts</li>
+                            <li>Engage students with award winning classic and contemporary authors such as Carl Sandburg, Robert Frost, Ray Bradbury, Annie Dillard, Ernest Hemingway and Elizabeth Bishop</li>
+                            <li>Multicultural selections from authors such as Toni Cade Bambara, Sandra Cisneros, Arthur Ashe, Amy Tan, Judith Ortiz Cofer and Langston Hughes</li>
+						</ul>
+						<p><a href="http://www.emcp.com/lib/assets/Mirrors-Windows-Range-of-Reading.pdf" target="_blank"><strong>Download the complete range of texts in grades 6-12</strong></a></p>
+					</div>
+			<!-- 2 -->
+					<h3 class="showSingle" target="2"><strong>2. Digital Learning Technologies</strong></h2>
+					<div id="div2" class="targetDiv" style="display:none;">
+						<ul>
+							<li>EMC Bookshelf<sup>TM</sup> provides multiplatform eBooks for all program components</li>
+							<li>Interactive eWorkbooks with student feedback in Bookshelf</li>
+							<li>Only program to offer ETS Criterion&reg; Online Essay Grader</li>
+							<li>EXAMVIEW&reg; Assessment Suite - customizable</li>
+							<li>Common Core PARCC &amp; SBA practice assessments online</li>
+							<li>Flipgrid<sup>TM</sup> online video discussion platform</li>
+							<li>Avenue<sup>TM</sup> online video oral assessment</li>
+						</ul>
+					</div>
+			<!-- 3 -->
+					<h3 class="showSingle" target="3"><strong>3. Integrated Language Arts Program Meets the English Standards of Learning and Curriculum Framework for Virginia Public Schools</strong></h2>
+					<div id="div3" class="targetDiv" style="display:none;">
+						<ul>
+							<li>Wide range of reading strategies applied to a variety of literary and informational texts.</li>
+							<li>Skill development such as Grammar & Style and Vocabulary & Spelling based on the selections are integrated within the units.</li>
+							<li>End-of-unit performance tasks in Writing, Speaking & Listening, Viewing, and Test Practice Performance Tasks to apply skills.</li>
+							<li>Skill development culminates at the end of each book with a Language Arts Handbook.</li>
+                            <li><i>Meeting the Standards</i> and <i>Exceeding the Standards</i> Resources provide interactive language arts activities online.</li>
+						</ul>
+					</div>
+			<!-- 4 -->
+					<h3 class="showSingle" target="5"><strong>4. Scaffolded Gradual Release of Responsibility Approach</strong></h2>
+					<div id="div5" class="targetDiv" style="display:none;">
+						<ul>
+							<li>Mirrors & Windows is built around 8 thematic genre-based units covering Fiction, Nonfiction, Poetry, Drama, and Folk Literature.</li>
+							<li>Each unit gives students a Gradual Release of Responsibility opportunity through three levels of reading support: selections of Guided Reading, Directed Reading, and Independent Reading.</li>
+							<li>Gradual Release uses scaffolding to model and apply close reading skills and strategies for comprehension and analysis.</li>
+						</ul>
+					</div>
+			<!-- 5 -->
+					<h3 class="showSingle" target="6"><strong>5. Range of Texts and Complexity</strong></h2>
+					<div id="div6" class="targetDiv" style="display:none;">
+						<ul>
+                            <li><i>Mirrors & Windows</i> provides a wide range of text types within grade level text complexity levels – all in student textbook</li>
+							<li>Selections in all levels are at or above the recommended Lexile range for each respective grade</li>
+							<li>Lexiles for every selection provided in ATE Scope & Sequence, prereading pages, and in Lesson Plans</li>
+							<li>Qualitative and quantitative text complexity levels with difficulty and ease consideration are provided.</li>
+							<li>Independent reading Access Edition novels and plays with built-in study apparatus</li>
+							<li>EMC E-Library with over 300 literary and nonfiction texts</li>
+						</ul>
+					</div>
+			<!-- 6 -->
+					
+					<h3 class="showSingle" target="4"><strong>6. Differentiated Instruction and ELL Support</strong></h2>
+					<div id="div4" class="targetDiv" style="display:none;">
+						<ul>
+							<li>Differentiated Instruction support in Teacher's Edition</li>
+							<li>Differentiated Instruction Resource for English Language Learners, Developing Readers, and Advanced Students</li>
+							<li>Unit Vocabulary & Spelling and Grammar & Style language development workshops in textbook and Exceeding the Standards Resources</li>
+							<li>Comprehensive Language Arts Handbook in student textbook as an additional resource</li>
+							<li>Audio recordings for all selections</li>
+							<li>Spanish summaries for Differentiated Instruction ELL selections in text and audio</li>
+							<li>Oral Fluency Assessments in Assessment Guide</li>
+							<li>Flipgrid<sup>&trade;</sup> online video discussion platform</li>
+							<li>Avenue<sup>&trade;</sup> online video oral assessment</li>
+							<li>Spanish language readers and novels for extended reading in the native language</li>
+						</ul>
+					</div>
+			<!-- 7 -->
+					<h3 class="showSingle" target="7"><strong>7. Comprehensive Assessments</strong></h2>
+					<div id="div7" class="targetDiv" style="display:none;">
+						<ul>
+							<li>Test Practice at end of textbook units</li>
+                            <li><i>Assessment Guide</i> includes formative assessments with remediation rubrics</li>
+							<li>Lesson and unit exams</li>
+							<li>Oral fluency assessments</li>
+							<li>EXAMVIEW&reg; customizable exams</li>
+							<li><i>Exceeding the Standards</i>: Test Practice Resource</li>
+							<li><i>Meeting the Standards</i> Unit Practice Tests</li>
+							<li><i>Common Core Assessment Practice</i> in PAARC and Smarter Balanced formats online</li>
+						</ul>
+					</div>
+			<!-- 8 -->
+					<h3 class="showSingle" target="8"><strong>8. Best Service and Value</strong></h2>
+					<div id="div8" class="targetDiv" style="display:none;">
+						<ul>
+							<li>Best personalized customer service and professional development at no cost</li>
+							<li>Best value: For one low price you will have a complete print and digital package including all print and digital supplements</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END NEW -->
+        <div class="redefining paddingtop40 light-grey-slice">
+            <div class="ninesixty">
+                <h2>Components</h2>
+                <div class="one-half first textalignleft">
+                    <p class="bold">For students:</p>
+                    <ul class="lineheight1p75">
+                        <li>Textbook &amp; eBook</li>
+                        <li>Meeting the Standards eWorkbook</li>
+                        <li>Exceeding the Standards eWorkbook</li>
+                        <li>Differentiated Instruction eWorkbook with Spanish Summaries</li>
+						<li>ETS Criterion<sup>&reg;</sup> Online Essay Grader</li>
+                        <li>Audio program in eBook</li>
+                        <li>Access Editions (including eBooks)</li>
+                    </ul>
+                </div>
+                <div class="one-half last textalignleft">
+                    <p class="bold">For teachers:</p>
+                    <ul class="lineheight1p75">
+                        <li>Teacher's Edition Textbook</li>
+                        <li>Enhanced Teacher's Edition eBook</li>
+                        <li>Program Planning Guide</li>
+                        <li>Assessment Guide with <span class="bold">EXAM</span>VIEW<sup>&reg;</sup></li>
+                        <li>Meeting the Standards, Exceeding the Standards, & Differentiated Instruction</li>
+                        <li>Criterion Online Essay Grader</li>
+                        <li>Access Editions Assessment Manuals</li>
+                    </ul>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        
+        
+        <div class="mw-mp-ebook paddingtop40 paddingbottom40">
+			<div class="ninesixty">
+				<div class="conectate-iculture-first">				
+                    <img src="<?php echo $server; ?>lib/images/mw-mp-ebooks.jpg" alt="Mirrors & Windows Multiplatform eBooks" />
+				</div>
+				<div class="conectate-iculture-second">
+                    <h3 class="conectate">Multiplatform eBooks on Bookshelf</h3>
+                    <p class="no-top">EMC's multiplatform eBooks connect students to literature and language arts in and out of the classroom.  With a touch of the finger, students are able to access every page of their textbooks.  Our eBooks are available for delivery on all laptops, desktops, and mobile devices, including the iPad,<sup>&copy;</sup>.</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>        
+        </div>
+		<div class="redefining light-grey-slice">
+			<div class="ninehundred no-bottom">
+				<div class="aligncenter">
+					<h2>Redefining seamless learning through technology.</h2>
+				</div>
+				
+				<p>Through our partnership with the University of Minnesota's Institute for Design Innovation (IDI), we are now offering award-winning, research-based learning platforms that enhance your face-to-face, blended, or online classroom.</p>
+			</div>
+			<div class="redefining-bg">
+				<div class="vimeo-modals-container">
+					<div class="avenue-play-button">
+						<div class="play-button">
+							<div class="icon-play"></div>
+						</div>
+					</div>
+					<div class="avenue-play-button-hover-background"></div>
+					<div class="flipgrid-play-button">
+						<div class="play-button">
+							<div class="icon-play"></div>
+						</div>
+					</div>
+					<div class="flipgrid-play-button-hover-background"></div>
+				</div>            
+            </div>
+			<div class="ninesixty">
+				<div class="avenue-flipgrid">
+					<div class="one-half first">
+						<p><span class="bold">Avenue&trade;</span> is an innovative e-assessment platform for building custom speaking and listening tasks to capture, evaluate, archive, and visualize progress of video-based learner performances in your language arts classroom.</p>
+						
+						<a href="<?php echo $server; ?>technology/avenue/" target="_blank" class="learn-more-link">Learn more about Avenue</a>
+					</div>
+					<div class="one-half last">
+						<p><span class="bold">Flipgrid&trade;</span> allows teachers to create grids of short discussion-style questions that students respond to through recorded videos. Use Mirrors & Windows essential questions to practice speaking and listening skills. Flipgrid boosts community and social presence in face-to-face, hybrid, and online classrooms.</p>
+						
+						<a href="<?php echo $server; ?>technology/flipgrid/" target="_blank" class="learn-more-link">Learn more about Flipgrid</a>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script>
+  $(function() {
+    $( "#accordion" ).accordion({
+      collapsible: true,
+	  active: false
+    });
+  });
+  </script>
+<?php include '../../includes/footer.php'; ?>
---
+++ mirrors-and-windows/va/resources/index.php
@@ -0,0 +1,69 @@
+
+<?php 
+	$custom_title = 'Virginia | Mirrors & Windows Resources | ';
+	include '../../../includes/header.php';
+?>
+	<div id="container" class="mw">
+		<div class="header-background-image mw-va-bg-resources">
+			<div id="topImg">
+			<!--
+				<div class="play-button">
+					<div class="icon-play"></div>
+				</div>
+                <div class="header-hero-hover"></div>
+			-->
+			</div>
+		</div>
+		<div class="header-top">
+			<?php include '../../../includes/nav-ca.php'; ?>
+		</div>
+	<!-- Sample Chapters -->
+		<div>
+			<div class="ninesixty"><h3 style="margin-top:505px;">Hello Virginia ELA Teachers. If you are purchasing an English Language Arts Program, here are some resources about EMC's Mirrors &amp; Windows: Connecting with Literature program.</h3></div>
+		</div>
+		<div style="margin-top:0;" class="grey-slice" style="padding:25px 0 !important;">
+			<div class="ninesixty">
+				<div>
+					<h2>Sample Chapters</h2>
+					<p class="no-top">Take a look at these sample chapters to see actual content of the Mirrors &amp; Windows textbook.<br /><br />
+						Click <a href="https://www.joomag.com/magazine/ela-sample-chapters-high-school/M0356198001458247991" target="_blank" class="learn-more-link">here</a> to see Sample Chapters
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	<!-- EMC Access Editions Novels -->
+		<div style="padding:25px 0 !important;">
+			<div class="ninesixty">
+				<div>
+                    <h2>EMC Access Editions Novels</h2>
+					<p class="no-top">Our Access Editions novels and full-length plays offer a wide range of extended independent reading. These literary works include a complete study apparatus 
+						with close reading support and related nonfiction and informational texts.<br /><br />
+						Click <a href="http://joom.ag/hGSp" target="_blank" class="learn-more-link">here</a> to learn more about our Access Editions
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	<!-- OK PASS Correlations -->
+        <div class="grey-slice" style="padding:25px 0 !important;">
+            <div class="ninesixty">
+                <h2>Virginia English Standards of Learning Correlations</h2>
+                <p>These standards map correlations list how our program aligns 100% with the Virginia Standards of Learning for grades 6-12.<br /><br />
+					Click <a href="https://www.joomag.com/magazine/virginia-standards/M0202256001458249305" target="_blank" class="learn-more-link">here</a> to see the VA English SOL Correlations
+				</p>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	<!-- Range of Selection -->
+        <div style="padding:25px 0 !important;">
+            <div class="ninesixty">
+                <h2>Range of Selections</h2>
+                <p>The Range of Readings lists the contents in each level by genre for our entire ELA Program.<br /><br />
+					Click <a href="http://joom.ag/BGSp" target="_blank" class="learn-more-link">here</a> to see the Range of Selections
+				</p>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	</div>
+<?php include '../../../includes/footer.php'; ?>
