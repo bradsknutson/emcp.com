a071a20ecd677ac6a04de82b2aeaa721015414bd
weinerdog <bmolina@emcp.com>
2016-03-31 11:55:55 -0500

    NV MW edits

---
+++ includes/nav-nv.php
@@ -0,0 +1,11 @@
+<?php 
+    // $server is defined in server.php 
+    // get_current() is defined in functions.php 
+    // $current is defined in functions.php 
+?>			
+    <div class="container">
+	
+		<a href="<?php echo $server; ?>"><img src="<?php echo $server; ?>lib/images/logo.png" alt="EMC" /></a>
+		
+	
+	</div><!-- end .container -->
---
+++ mirrors-and-windows/nv/resources/index.php
@@ -0,0 +1,71 @@
+
+<?php 
+	$custom_title = 'Nevada | Mirrors & Windows Resources | ';
+	include '../../../includes/header.php';
+?>
+	<div id="container" class="mw">
+		<div class="header-background-image mw-nv-bg-resources">
+			<div id="topImg">
+			<!--
+				<div class="play-button">
+					<div class="icon-play"></div>
+				</div>
+                <div class="header-hero-hover"></div>
+			-->
+			</div>
+		</div>
+		<div class="header-top">
+			<?php include '../../../includes/nav-nv.php'; ?>
+		</div>
+	<!-- Sample Chapters -->
+		<div>
+			<div class="ninesixty"><h3 style="margin-top:505px;">Hello Nevada ELA Teachers. If you are purchasing an English Language Arts Program, here are some resources about EMC's Mirrors &amp; Windows: Connecting with Literature program.</h3></div>
+		</div>
+		<div style="margin-top:0;" class="grey-slice" style="padding:25px 0 !important;">
+			<div class="ninesixty">
+				<div>
+					<h2>Sample Chapters</h2>
+					<p class="no-top">Take a look at these sample chapters to see actual content of the Mirrors &amp; Windows textbook. 
+						The Unit 1 Fiction sample is from Level II Grade 7. The Unit 3 Nonfiction sample is from Level III Grade 8.<br /><br />
+						Click <a href="https://www.joomag.com/magazine/california-ela-sample-chapters/M0709333001447708220" target="_blank" class="learn-more-link">here</a> to see Sample Chapters
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	<!-- EMC Access Editions Novels -->
+		<div style="padding:25px 0 !important;">
+			<div class="ninesixty">
+				<div>
+                    <h2>EMC Access Editions Novels</h2>
+					<p class="no-top">Our Access Editions novels and full-length plays offer a wide range of extended independent reading. These literary works include a complete study apparatus 
+						with close reading support and related nonfiction and informational texts.<br /><br />
+						Click <a href="http://joom.ag/hGSp" target="_blank" class="learn-more-link">here</a> to learn more about our Access Editions.<br />
+						Click <a href="https://www.joomag.com/magazine/ela-sample-chapters-middle-school/M0709333001447708220" target="_blank" class="learn-more-link">here</a> to see samples of our Access Editions.
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	<!-- OK PASS Correlations -->
+        <div class="grey-slice" style="padding:25px 0 !important;">
+            <div class="ninesixty">
+                <h2>Nevada Academic Content Standards Correlations</h2>
+                <p>These standards map correlations list how our program aligns 100% with the Nevada Academic Content Standards for Language Arts.<br /><br />
+					Click <a href="https://www.joomag.com/magazine/nevada-standards/M0693762001459268972" target="_blank" class="learn-more-link">here</a> to see the NACS Correlations
+				</p>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	<!-- Range of Selection -->
+        <div style="padding:25px 0 !important;">
+            <div class="ninesixty">
+                <h2>Range of Selections</h2>
+                <p>The Range of Readings lists the contents in each level by genre for our entire ELA Program.<br /><br />
+					Click <a href="http://joom.ag/BGSp" target="_blank" class="learn-more-link">here</a> to see the Range of Selections
+				</p>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+	</div>
+<?php include '../../../includes/footer.php'; ?>
