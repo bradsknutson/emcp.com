58450c1498263d027464b2175facdcce6977810f
Brad Knutson <brad.s.knutson@gmail.com>
2016-05-05 15:24:43 -0500

    Preliminary changes to Readers Page

--- world-languages/readers/index.php
+++ world-languages/readers/index.php
@@ -11,24 +11,25 @@
 		<div class="header-top">
 			<?php include '../../includes/nav.php'; ?>
 		</div>
-		<div class="secondary-first white">
+		<div class="secondary-first light-grey-slice">
 			<div class="ninesixty">
+                <h3>Enhance your students’ learning experience with leveled readers in the target language. Readers help develop students’ grammar and vocabulary knowledge while building their ability to comprehend increasingly difficult pieces of literature.</h3>
+            </div>
+        </div>
+		<div class="secondary-first white margintop0">
+			<div class="ninesixty paddingtop0i">
 				<div class="conectate-left margintop40">
-                    <h2>EMC Enhanced eReaders</h2>
-					<p class="no-top">Enhanced eReaders connect students to their books in and out of the classroom. eReaders work on all laptops, desktops, and mobile devices including the iPad<sup>&copy;</sup>. eReaders are available in Spanish, French and German.</p>
-                    
-                    <p>Features:</p>
+                    <h2>Enhanced eReaders</h2>
+					<p class="no-top">Enhanced eReaders with beautiful illustartions to help develop reading and listening skills through texts and sincronized audio recordings of the stories, consolidate vocabulary and learn culture. These eReaders are multi-platform, they work on all laptops, desktops, and mobile devices including the iPad<sup>&copy;</sup>.</p>
                     
                     <ul>
-                        <li>Beautiful illustrations</li>
-                        <li>Synchronized audio</li>
-                        <li>Note taking</li>
-                        <li>Interactive activities</li>
-                        <li>Games</li>
+                        <li>Available in Spanish, French, German and Italian</li>
+                        <li>Paperback only</li>
                     </ul>
                     
-                    <p><a class="learn-more-link smaller-link" href="<?php echo $server; ?>world-languages/readers/buy/">Buy Now!</a></p>
-                    <p><a class="learn-more-link smaller-link" href="<?php echo $server; ?>ereaders/">Learn more!</a></p>
+                    <a class="technology-buttons wl-readers-buttons transition" href="<?php echo $server; ?>world-languages/readers/buy/" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','World Language Readers Buy Now');">Buy Now</a>
+                    <a class="technology-buttons wl-readers-buttons transition" href="<?php echo $server; ?>ereaders/" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','World Language Readers Learn More');">Learn More</a>
+                    
 				</div>
 				<div class="conectate-right">
 					<img src="<?php echo $server; ?>lib/images/readers-example.png" alt="EMC Enhanced eReaders" style="margin-top:40px !important;"/>
@@ -38,11 +39,22 @@
         </div>        
         <div class="redefining no-bottom light-grey-slice">
             <div class="ninesixty paddingtop40 paddingbottom40">
-                <img src="<?php echo $server; ?>lib/images/mlt-reader.png" alt="My Language Town" class="img-float-left mlt-img" />
-                <h2>My Language Town</h2>
-                <p>A book series packed with communicative dialogs and a collection of songs for Spanish language learners.</p>
-                
-                <p><a class="learn-more-link smaller-link" href="<?php echo $server; ?>mylanguagetown/">Learn more</a></p>
+                <div style="width:440px;float: left;">
+                    <img src="<?php echo $server; ?>lib/images/mlt-reader.png" alt="My Language Town" class="img-float-left mlt-img" />
+                </div>
+                <div style="width:460px;float: left;">
+                    <h2>My Language Town Readers and Songs</h2>
+                    <p>Original Spanish eReaders series packed with communicative dialogs and activities and a collection of songs for mastering vocabulary, grammar and culture. Designed for use both in the classroom and as an independent study, My Language Town materials can make learning Spanish language an interactive, engaging experience.</p>
+
+                    <ul>
+                        <li>Available in Spanish, French, German and Italian</li>
+                        <li>Digital and paperback (on demand only)</li>
+                        <li>Songs</li>
+                    </ul>
+
+                    <a class="technology-buttons wl-readers-buttons transition" href="<?php echo $server; ?>mylanguagetown/" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','My Language Town Learn More');">Learn More</a>
+                </div>
+                <div class="clearfix"></div>
                 
             </div>
         </div>
@@ -52,7 +64,7 @@
                 <img src="<?php echo $server; ?>lib/images/cideb-readers-v2.png" alt="CIDEB Readers" class="img-float-right cideb-img" />
                 <p>Prefer the no-power-required type of reader? You’re at the right place. Either as stand-alone books or as extensions to our textbook series, readers develop your learners' grammar and vocabulary while building their ability to comprehend increasingly difficult pieces of literature.</p>
                 
-                <p><a class="learn-more-link smaller-link" href="<?php echo $server; ?>world-languages/readers/paperback/buy/">Buy Now!</a></p>
+                <a class="technology-buttons wl-readers-buttons transition" href="<?php echo $server; ?>world-languages/readers/paperback/buy/" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','EMC Paperback Readers Buy Now');">Buy Now</a>
                 <div class="clearfix"></div>
             </div>
         </div>
