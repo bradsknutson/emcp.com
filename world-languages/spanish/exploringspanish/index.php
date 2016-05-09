725ad7e8b6d685b170c79ead1aaa82729b434dd4
weinerdog <bmolina@emcp.com>
2016-05-09 08:52:24 -0500

    Exploring Spanish Landing page

---
+++ world-languages/spanish/exploringspanish/index.php
@@ -0,0 +1,121 @@
+<?php 
+	$custom_title = 'Exploring Spanish | ';
+	include '../../../includes/header.php';
+?>
+<style>
+.ui-accordion .ui-accordion-header {
+	background: #F0EFEB !important;
+	font-size: 22px !important;
+	padding-right: .2em !important;
+}
+.redefining p {
+	font-size: 20px !important;
+}
+.ui-helper-reset {
+	font-size: 18px !important;
+}
+.ui-widget {
+	font-family: "myriad-pro", sans-serif !important;
+}
+.ui-accordion-content {
+    height: 550px !important;
+}
+</style>
+	<div id="container" class="quechevere">
+		<div class="vimeo-modal"></div>
+		<div class="header-background-image exploring-spanish-bg">
+			<div id="topImg"></div>
+			<div class="colorBar" style="background:#c10006; height:10px; width:100%; margin-top:-37px; position:relative; top:35px;"></div>
+		</div>
+		<div class="header-top">
+			<?php include '../../../includes/nav.php'; ?>
+		</div>
+		<div class="secondary-first">
+			<div class="ninesixty">
+				<div class="ex-sp-left">
+                    <p class="no-top">
+					Set your students on a path to discovering and learning languages with EMC’s Exploring Spanish, an introductory program intended to present Spanish language and 
+					culture to young, beginning language learners. It is perfect for use in middle school FLEX and FLES programs. It can also be used as part of a formal 7th and 8th 
+					grade language course or as supplementary material in a first-year high school class. With Exploring Spanish your students will get a taste of the Spanish language 
+					and culture through 20 thematic units covering topics of high interest: food, music, art, literature, geography, and so much more! Engage them in listening, 
+					speaking, reading, and writing Spanish while using functional and relevant vocabulary. 
+					</p>
+				</div>
+				<div class="ex-sp-right">
+					<img src="<?php echo $server; ?>lib/images/Exploring-Spanish-iPad.png" alt="Exploring Spanish Program" />
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="ninesixty no-bottom no-top">
+            <div class="tech-button-container" style="margin:50px 0 0 25px;">
+				<a class="technology-buttons transition" href="http://edu.emcp.com/world-languages/exploring-spanish-third-edition-revised.html" target="_blank" onClick="ga('send', 'event', 'Call To Action Button','click','Exploring Spanish Sample');" style="position:relative !important;display:inline !important;">Buy Now</a>
+            </div>
+        </div>
+		<div class="ninehundred">
+			<div class="sub-featured onesix black">
+				<h3>New! for Third Edition Revised</h3>
+				<p>
+					<ul>
+						<li>New! The updated <i>Realia</i> section in each unit gives students additional activities for practice.</li>
+						<li>New! Provides students a sampling of the food, art, music, and literature of Spanish-speaking countries with current, relevant topics.</li>
+						<li>Builds skills in speaking, listening, reading, and writing.</li>
+					</ul>
+				</p>
+			</div>
+			<div class="sub-featured onesix black">
+				<p>
+					<ul>
+						<li>Symtalk&reg; activities duplicate the natural process of language acquisition through the use of visual image symbol cards (160 digital cards).</li>
+						<li>Puzzles and other fun activities engage and entertain students.</li>
+						<li>New! Multiplatform eBook and eWorkbook are available for delivery on all laptops, desktops, and mobile devices and feature audio and interactive activities.</li>
+					</ul>
+				</p>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="ex-sp-bg">
+			<div class="ninehundred">
+				<div class="sub-featured onesix black">
+					<p class="no-top">
+					<strong>Program Resources</strong><br />
+					Exploring Spanish Student &copy;2017<br />
+					Set your students on a path to discovering and learning languages with
+					EMC's Exploring Spanish, an introductory program intended to present
+					Spanish language and culture to young, beginning language learners.<br /><br />
+
+					<strong>Exploring Spanish Workbook &copy;2017</strong>
+					<ul>
+						<li>Builds skills in speaking, listening, reading, and writing</li>
+					</ul>
+				</div>
+				<div class="sub-featured onesix black">
+					<strong>Exploring Spanish Teacher's Guide &copy;2017</strong><br />
+					The Exploring Spanish Teacher's Guide contains:
+					<ul>
+						<li>Unit Objectives, Lesson Plans, Teaching Suggestions, and Symtalk Activities</li>
+						<li>Textbook Answer Key</li>
+						<li>Workbook Answer Key</li>
+						<li>Check-up Written Activities Blackline Masters and Answer Key</li>
+						<li>Check-up Listening Activities Blackline Masters and Answer Key</li>
+						<li>Textbook Audio Program Script</li>
+					</ul>
+					<strong>The Exploring Spanish Teacher’s Guide eBook also includes access to the following:</strong>
+					<ul>
+						<li>eWorkbook</li>
+						<li>Textbook Audio Program files</li>
+						<li>Check-up Listening Activities Audio files</li>
+						<li>Symtalk Digital Symbol cards</li>
+						<li>eVisuals</li>
+					</ul>
+					</p>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<?php include '../../../includes/footer.php'; ?>
