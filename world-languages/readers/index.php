7c591241d981e410473d53e3dac3d3613bfcacd6
weinerdog <bmolina@emcp.com>
2016-05-26 15:52:08 -0500

    WL Readers page update

--- world-languages/readers/index.php
+++ world-languages/readers/index.php
@@ -16,7 +16,29 @@
                 <h3>Enhance your students' learning experience with leveled readers in the target language. Readers help develop students’ grammar and vocabulary knowledge while building their ability to comprehend increasingly difficult pieces of literature.</h3>
             </div>
         </div>
-		<div class="secondary-first white margintop0">
+		<div class="redefining no-bottom">
+            <div class="ninesixty paddingtop40 paddingbottom40">
+                <div style="width:440px;float: left;">
+                    <img src="<?php echo $server; ?>lib/images/mlt-reader.png" alt="My Language Town" class="img-float-left mlt-img" />
+                </div>
+                <div style="width:460px;float: left;">
+                    <h2>My Language Town Readers and Songs</h2>
+                    <p>Original Spanish eReaders series packed with communicative dialogs and activities and a collection of songs for mastering vocabulary, grammar and culture. Designed for use both in the classroom and as an independent study, My Language Town materials can make learning Spanish language an interactive, engaging experience.</p>
+
+                    <ul>
+                        <li>Available in Spanish (August 1st)</li>
+                        <li>Digital and paperback (on demand only)</li>
+                        <li>Songs</li>
+                    </ul>            
+                    
+                    <a class="technology-buttons wl-readers-buttons transition" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00eafa7e88f41d8cbd96a0269d731b013d8d034e9a8d6f0718f7" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','My Language Town Preorder Sample');">Sample</a>
+                    <a class="technology-buttons wl-readers-buttons transition" href="<?php echo $server; ?>mylanguagetown/" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','My Language Town Learn More');">Learn More</a>
+                </div>
+                <div class="clearfix"></div>
+                
+            </div>
+        </div>
+		<div class="secondary-first light-grey-slice margintop0">
 			<div class="ninesixty paddingtop0i">
 				<div class="conectate-left margintop40">
                     <h2>CIDEB Enhanced eReaders</h2>
@@ -39,28 +61,6 @@
 				<div class="clearfix"></div>
 			</div>
         </div>        
-        <div class="redefining no-bottom light-grey-slice">
-            <div class="ninesixty paddingtop40 paddingbottom40">
-                <div style="width:440px;float: left;">
-                    <img src="<?php echo $server; ?>lib/images/mlt-reader.png" alt="My Language Town" class="img-float-left mlt-img" />
-                </div>
-                <div style="width:460px;float: left;">
-                    <h2>My Language Town Readers and Songs</h2>
-                    <p>Original Spanish eReaders series packed with communicative dialogs and activities and a collection of songs for mastering vocabulary, grammar and culture. Designed for use both in the classroom and as an independent study, My Language Town materials can make learning Spanish language an interactive, engaging experience.</p>
-
-                    <ul>
-                        <li>Available in Spanish (August 1st)</li>
-                        <li>Digital and paperback (on demand only)</li>
-                        <li>Songs</li>
-                    </ul>            
-                    
-                    <a class="technology-buttons wl-readers-buttons transition" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00eafa7e88f41d8cbd96a0269d731b013d8d034e9a8d6f0718f7" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','My Language Town Preorder Sample');">Sample</a>
-                    <a class="technology-buttons wl-readers-buttons transition" href="<?php echo $server; ?>mylanguagetown/" target="_blank" onclick="ga('send', 'event', 'Call To Action Button','click','My Language Town Learn More');">Learn More</a>
-                </div>
-                <div class="clearfix"></div>
-                
-            </div>
-        </div>
         <div class="redefining no-bottom">
             <div class="ninesixty paddingtop40 paddingbottom40">
                 <h2>EMC Paperback Readers</h2>
