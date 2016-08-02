0b5fbdb35aa7a6ea74bd477f3c2e15b10638f299
Brad Knutson <brad.s.knutson@gmail.com>
2016-08-02 11:56:04 -0500

    Updates to Exploring Series page

--- world-languages/spanish/exploringspanish/index.php
+++ world-languages/spanish/exploringspanish/index.php
@@ -1,5 +1,5 @@
 <?php 
-	$custom_title = 'Exploring Spanish | ';
+	$custom_title = 'Exploring Series | ';
 	include '../../../includes/header.php';
 ?>
 <style>
@@ -33,13 +33,7 @@
 		<div class="secondary-first">
 			<div class="ninesixty">
 				<div class="ex-sp-left">
-                    <p class="no-top">
-					Set your students on a path to discovering and learning languages with EMC’s Exploring Spanish, an introductory program intended to present Spanish language and 
-					culture to young, beginning language learners. It is perfect for use in middle school FLEX and FLES programs. It can also be used as part of a formal 7th and 8th 
-					grade language course. With Exploring Spanish your students will get a taste of the Spanish language 
-					and culture through 20 thematic units covering topics of high interest: food, music, art, literature, geography, and so much more! Engage them in listening, 
-					speaking, reading, and writing Spanish while using functional and relevant vocabulary. 
-					</p>
+                    <p class="no-top">Set your students on a path to discovering and learning languages with EMC’s Exploring Series, introductory programs intended to present language and culture to young, beginning language learners. The Exploring Series is perfect for use in middle school FLEX and FLES programs. It can also be used as part of a formal 7th and 8th grade language course. With Exploring your students will get a taste of language and culture through 20 thematic units covering topics of high interest: food, music, art, literature, geography, and so much more! Engage them in listening, speaking, reading, and writing while using functional and relevant vocabulary.</p>
 				</div>
 				<div class="ex-sp-right">
 					<img src="<?php echo $server; ?>lib/images/exploring-spanish-cover.png" alt="Exploring Spanish Program" />
@@ -47,6 +41,7 @@
 				<div class="clearfix"></div>
 				<div class="tech-button-container" style="margin:20px 0 0;">
 					<a class="technology-buttons transition" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00ea95ac29134a86ce5742d40faca4d666daee93197503b0ef54" target="_blank" onClick="ga('send', 'event', 'Call To Action Button','click','Exploring Spanish Sample');" style="position:relative !important;display:inline !important;">Sample</a>
+                    <p>The Exploring Series is available for Spanish, French, German, Chinese, Italian and Arabic. <a class="learn-more-link" href="http://edu.emcp.com/world-languages.html?emc_product_series=386">Click here to learn more</a> about all of the languages offered.</p>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -55,16 +50,35 @@
             <div class="ninesixty paddingbottom40" style="margin-top:15px;">
 				<div id="accordion">
 			<!-- 1 -->
-					<h3 class="showSingle" target="1"><strong>New! for Third Edition Revised</strong></h3>
+					<h3 class="showSingle" target="1"><strong>New! For Spanish and French Revised Editions</strong></h3>
 					<div id="div1" class="targetDiv">
-                        <ul class="sp-landing">
-							<li>New! The updated <i>Realia</i> section in each unit gives students additional activities for practice.</li>
-							<li>New! Provides students a sampling of the food, art, music, and literature of Spanish-speaking countries with current, relevant topics.</li>
-							<li>Builds skills in speaking, listening, reading, and writing.</li>
-							<li>Symtalk&reg; activities duplicate the natural process of language acquisition through the use of visual image symbol cards (160 digital cards).</li>
-							<li>Puzzles and other fun activities engage and entertain students.</li>
-							<li>New! Multiplatform eBook and eWorkbook are available for delivery on all laptops, desktops, and mobile devices and feature audio and interactive activities.</li>
-						</ul>
+                        <p>For Students:</p>
+                        <ul>
+                            <li>Student Edition Textbook</li>
+                            <li>Student Edition eBook</li>
+                            <li>Student Edition Workbook</li>
+                            <li>Student Edition eWorkbook</li>
+                        </ul>
+                        <p>For Teachers:</p>
+                        <ul>
+                            <li>Teacher's Guide, which includes:</li>
+                            <li>Unit Objectives, Lesson Plans, Teaching Suggestions, and Symtalk Activities</li>
+                            <li>Textbook Answer Key</li>
+                            <li>Workbook Answer Key</li>
+                            <li>Check-up Written Activities Blackline Masters and Answer Key</li>
+                            <li>Check-up Listening Activities Blackline Masters and Answer Key</li>
+                            <li>Textbook Audio Program Script</li>
+                            <li>Exploring Teacher's Guide eBook, which also includes access to:
+                                <ul>
+                                    <li>eWorkbook</li>
+                                    <li>Textbook Audio Program files</li>
+                                    <li>Check-up Listening Activities Audio files</li>
+                                    <li>Symtalk Digital Symbol cards</li>
+                                    <li>eVisuals</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <p>Not every component is available for every language. See <a href="<?php echo $server; ?>exploring">emcp.com/exploring</a> for more details.</p>
 					</div>
 			<!-- 2 -->
 					<h3 class="showSingle" target="2"><strong>Program Resources</strong></h3>
