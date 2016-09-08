d7b1eacade54a7a82c2d3b7389b130220cdffef6
weinerdog <bmolina@emcp.com>
2016-09-08 14:42:25 -0600

    Update to

--- texas/resources/index.php
+++ texas/resources/index.php
@@ -1,5 +1,38 @@
-
 <?php 
-	$custom_title = 'Texas Resources | Mirrors & Windows | ';
+	$custom_title = 'Texas Resources | Proclamation 2017 | ';
 	include '../../includes/header.php';
 ?>
+	<div id="container" class="texas-resources">
+		<div class="header-background-image texas-resources-bg">
+			<div id="topImg"></div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="white-slice al-top-content">
+            <div class="seveneighty">
+                <p>Hello Texas Teachers. Welcome to our Proclamation 2017 Resources site. EMC offers Spanish, French, German, and Chinese Language Programs, all of which are available in our award winning digital platform Passport®.  Feel free to look at our resources or go to our Texas Proclamation 2017 Homepage for more information and to request samples.</p>
+
+                <h2>Passport</h2>
+                <p>There is a lot that could be said about Passport, but its even better to experience it: http://www.emcp.com/passport/tour/</p>
+
+                <h2>Proclamation 2017 Approved Language Programs</h2>
+                <p><a href="https://www.joomag.com/magazine/texas-adoption-program-overviews/M0939966001459540031" target="_blank" class="learn-more-link">See the detailed Program overview for our Spanish and French programs.</a></p>
+
+                <h2>Available Languages Overview Brochures 
+                <p>See our Program Overview Brochures here: https://www.joomag.com/magazine/emc-world-language-brochures/M0396129001457106036
+                Exploring Spanish (still needed)</p>
+
+                <h2>TEKS
+                Click here to download the TEKS correlations.</p>
+
+                <h2>Extended reading support
+                <p>Enhance your students' learning experience with leveled readers in the target language. Readers help develop students’ grammar and vocabulary knowledge while building their ability to comprehend increasingly difficult pieces of literature.</p>
+
+                <p><a href="/world-languages/readers/" class="learn-more-link">Click here to learn more about readers.</a></p>
+
+            </div>
+        </div>
+    </div>
+
+<?php include '../../includes/footer.php'; ?>
