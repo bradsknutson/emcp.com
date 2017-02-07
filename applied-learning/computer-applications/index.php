df8ac713a1bd10fe4757c235c8a121a9f40f0548
weinerdog <bmolina@emcp.com>
2017-02-07 11:35:57 -0700

    CA page updates to 2016 Books

--- applied-learning/computer-applications/index.php
+++ applied-learning/computer-applications/index.php
@@ -13,33 +13,20 @@
 		<div class="white-slice al-top-content">
 			<div class="ninesixty">
                 <div class="left-slice">
-                    <img src="<?php echo $server; ?>lib/images/al-ca-mso13.png" alt="A Guide to Microsoft Office 2013" />
+                    <h1>Guided, Step-by-Step Approach</h1>
+                    <p><em>Guidelines for Microsoft Office 2016</em> offers clear, easy-to-follow instructions with quick tips for potential trouble spots with minimal reading and maximum visuals.</p>
+                    <a class="learn-more-link" href="<?php echo $server; ?>applied-learning/computer-applications/guidelines-2013/">Buy It Now!</a>
                 </div>
                 <div class="right-slice">
-                    <h1>Business Projects Approach</h1>
-                    <p>Teach your students the fundamentals of Microsoft Office 2013 with hands-on practice and cross-curricular projects for real-world, business related experiences.</p>
-                    <a class="learn-more-link" href="<?php echo $server; ?>applied-learning/computer-applications/guide-to-mso-2013/">Buy It Now!</a>
+                    <img src="<?php echo $server; ?>lib/images/al-ca-guidelines2016.png" alt="Guidelines for Office 2016" />
                 </div>
                 <div class="clearfix"></div>
             </div>
         </div>
 		<div class="grey-slice">
-			<div class="ninesixty slice-odd">
-                <div class="left-slice">
-                    <h1>Guided, Step-by-Step Approach</h1>
-                    <p><em>Guidelines for Microsoft Office 2013</em> offers clear, easy-to-follow instructions with quick tips for potential trouble spots with minimal reading and maximum visuals.</p>
-                    <a class="learn-more-link" href="<?php echo $server; ?>applied-learning/computer-applications/guidelines-2013/">Buy It Now!</a>
-                </div>
-                <div class="right-slice">
-                    <img src="<?php echo $server; ?>lib/images/al-ca-guidelines2013.png" alt="Guidelines for Office 2013" />
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-		<div class="white-slice">
 			<div class="ninesixty">
                 <div class="left-slice">
-                    <img src="<?php echo $server; ?>lib/images/al-ca-marquee2013.png" alt="Marquee Office 2013" />
+                    <img src="<?php echo $server; ?>lib/images/al-ca-marquee2016.png" alt="Marquee Office 2016" />
                 </div>
                 <div class="right-slice">
                     <h1>Visual Approach</h1>
@@ -49,7 +36,7 @@
                 <div class="clearfix"></div>
             </div>
         </div>
-		<div class="grey-slice">
+		<div class="white-slice">
 			<div class="ninesixty">
                 <div class="left-slice">
                     <h1>Certification, Cased-based Approach</h1>
@@ -57,20 +44,7 @@
                     <a class="learn-more-link" href="<?php echo $server; ?>applied-learning/computer-applications/benchmark-2013/">Buy It Now!</a>
                 </div>
                 <div class="right-slice">
-                    <img src="<?php echo $server; ?>lib/images/al-ca-benchmark-2013.png" alt="Benchmark Office 2013" />
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-		<div class="white-slice">
-			<div class="ninesixty">
-                <div class="left-slice">
-                    <img src="<?php echo $server; ?>lib/images/al-ca-sig2013.png" alt="Signature Word 2013" />
-                </div>
-                <div class="right-slice">
-                    <h1>Comprehensive Approach</h1>
-                    <p>Prepare your students for the Word 2013 skills required in college and the job market with <em>Signature Series: Microsoft Word 2013</em></p>
-                    <a class="learn-more-link" href="<?php echo $server; ?>applied-learning/computer-applications/sig-word-2013/">Buy It Now!</a>
+                    <img src="<?php echo $server; ?>lib/images/al-ca-benchmark-2016.png" alt="Benchmark Office 2016" />
                 </div>
                 <div class="clearfix"></div>
             </div>
