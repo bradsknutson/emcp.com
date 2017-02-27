0ce812dac274c41b3e31a944b3b622b5d7d8ecc8
Brian Molina <bmolina@emcp.com>
2017-02-27 16:15:13 -0700

    Grand Prairie landing page additions
    Grand Prairie landing page additions

---
+++ grandprairie/resources/index.php
@@ -0,0 +1,36 @@
+<?php 
+	$custom_title = 'Grand Prairie Resources | Proclamation 2017 | ';
+	include '../../includes/header.php';
+?>
+	<div id="container" class="grand-prairie-resources">
+		<div class="header-background-image grand-prairie-resources-bg">
+			<div id="topImg">
+				<div style="padding-top:525px;"><a class="mw-buttons" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00ea34e9212ae851742777d6f33a2e3ce90d16c89036f0eaf1aa" target="_blank">Sample</a></div>
+			</div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="white-slice al-top-content paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <p>Hello, Grand Prairie teachers! Welcome to EMC’s engaging Spanish and French programs. Click on the links below to take a tour of our fantastic content and our state of the art, unique in the market online learning environment, Passport<sup>&reg;</sup>.</p>
+            </div>
+        </div>
+        <div class="grey-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h3>Passport</h3>
+                <p>There is a lot that could be said about Passport, but it’s even better to <a href="/passport/tour/" target="_blank" class="learn-more-link">experience it</a>.</p>
+				<p>Watch our Passport Walk-through video <a href="https://vimeo.com/191874819" target="_blank" class="learn-more-link">here</a>. 
+				<p>Need to know more? Go to our <a href="http://support.emcschool.com/product/passport/" target="_blank" class="learn-more-link">Passport support site</a> to watch in-depth videos and tutorials</p>
+            </div>
+        </div>
+        <div class="white-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h3>Spanish and French</h3>
+                <p>Take a unit tour of our Spanish program <a href="https://vimeo.com/200376096" target="_blank" class="learn-more-link"><span class="italic">&iexcl;Qu&eacute; ch&eacute;vere!</span></a></p>
+				<p>Take a unit tour of our French program <a href="https://vimeo.com/200708010" target="_blank" class="learn-more-link"><span class="italic">T'es branch&eacute;?!</span></a></p>
+            </div>
+        </div>
+    </div>
+
+<?php include '../../includes/footer.php'; ?>
