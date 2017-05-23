c34d6229b5b335ccb5ebc4156e28fb27c7a4272c
Brian Molina <bmolina@emcp.com>
2017-05-23 12:51:05 -0600

    Texas bogo request thank you page

---
+++ thank-you/texas-bogo/index.php
@@ -0,0 +1,16 @@
+<?php 
+	$custom_title = 'Digital Sampler | ';
+	include '../../includes/header.php';
+?>
+	<div id="container" class="technology">
+		<div class="header-secondary-page"></div>
+		<div class="header-top-dark">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="world-languages-title">
+			<div class="ninehundred onesix fonttwenty">
+				<h3>Thank you! Your account manager will contact you shortly to complete your order.</h3>
+			</div>
+		</div>
+		
+<?php include '../../includes/footer.php'; ?>		
