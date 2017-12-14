c1c2953666a88810a935c0001a462626167b4d2f
Brian Molina <bmolina@emcp.com>
2017-12-14 12:13:11 -0700

    ELA page

---
+++ texasela/index.php
@@ -0,0 +1,70 @@
+
+<?php 
+	$custom_title = 'Texas | ELA | ';
+	include '../includes/header.php';
+?>
+	<div id="container" class="texas-landing-ela">
+		<div class="header-background-image texas-landing-bg">
+			<div id="topImg">
+				<!--
+					<div style="padding-top:525px;"><a class="mw-buttons" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00ea34e9212ae851742777d6f33a2e3ce90d16c89036f0eaf1aa" target="_blank">Sample</a></div>
+				-->
+			</div>
+		</div>
+		<div class="header-top">
+			<?php include '../includes/nav.php'; ?>
+		</div>
+		<div class="white-slice top-slice">
+            <div class="seveneighty">
+                <h2 class="purple">Introducing Mirrors &amp; Windows</h2>
+                <p>EMC School is proud to present its exciting literature and language arts program, Mirrors &amp; Windows. 
+				This seven-level program is built on a collection of rich, diverse, and timeless writings by renowned, award-winning authors.
+				</p>
+
+				<p>Mirrors &amp; Windows challenges students to reach their maximum potential while differentiating instruction for individual learners. 
+				Using a gradual release approach to reading, students will achieve a deep comprehension of the material and a greater appreciation of the 
+				literary genres. They will learn to recognize and make connections between the selections, the world, and themselves. Along the way, they 
+				will discover a love of literature that will grow throughout their lives. Truly, literature will turn mirrors into windows for your students.
+				</p>
+            </div>
+        </div>
+		<div class="grey-slice paddingtop20 paddingbottom40">
+            <div class="seveneighty">
+                <p> Among its extensive supplemental materials a variety of Texas authors including:</p>
+				<strong>Level I/Grade 6</strong>
+				<ul>
+					<li><strong>Pat Mora</strong> – author of "Same Song" and "Gold" – from El, Paso, TX</li>
+					<li><strong>Robert Fulghum</strong> – author of "All I Really Need to Know I Learned in Kindergarten" – from Waco, TX</li>
+					<li><strong>Carmen Tafolla</strong> – author of "Mi Familia" – from San Antonio, TX</li>
+				</ul>
+				<strong>Level II/Grade 7</strong>
+				<ul>
+					<li><strong>Tomas Rivera</strong> – author of "The Portrait" – from Crystal City, TX (passed away in 1984)</li>
+					<li><strong>Naomi Shihab Nye</strong> – author of "The Lost Parrot" –lives in San Antonio, TX</li>
+				</ul>
+				<strong>Level III/Grade 8</strong>
+				<ul>
+					<li><strong>Angela Shelf Medearis</strong> – author of "I Was Born at the Wrong Time" –lives in Austin, TX</li>
+					<li><strong>Roberto Felix Salazar</strong> – author of "The Other Pioneers" –from Laredo, TX</li>
+				</ul>
+            </div>
+        </div>
+		<div class="white-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h2 class="purple">It's here!</h2>
+                <p>EMC School's English Language Arts program Mirrors &amp; Windows has arrived on our state-of-the-art learning platform Passport&reg;</p>
+
+				<p>Every language arts publisher can say they have great literature, but only <strong>EMC School has Passport&reg;</strong><br />
+				Built from the ground up, EMC School has uniquely designed Passport&reg; for your language arts classroom. It’s built to be intuitive and 
+				extremely user-friendly while the social media look and feel instantly clicks with students. Easy-to-use, Passport&reg; helps students focus 
+				more on the content while the discussion tools engage and help them to dive deeper into the curriculum and connect with fellow classmates.</p>
+
+				<p>Passport&reg; is real-world! The ELA curriculum included teaches students useful skills to prepare them for college and future careers.<br />
+				For teaching professionals: Mirrors &amp; Windows with Passport&reg; allows teachers easy access to all program resources. It provides a way to 
+				track the progress of all students visually with stunning graphics and charts. It also gives teachers a straightforward way to assign homework 
+				and tasks as well as provide feedback to students.
+				</p>
+            </div>
+        </div>
+    </div>
+<?php include '../includes/footer.php'; ?>
