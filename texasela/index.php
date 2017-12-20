f042a378d225e6f1dd5c305bcc5e27703c8d638a
Brian Molina <bmolina@emcp.com>
2017-12-20 13:47:10 -0700

    Texas ELA Landing Page
    Texas ELA Landing Page

--- texasela/index.php
+++ texasela/index.php
@@ -14,57 +14,117 @@
 		<div class="header-top">
 			<?php include '../includes/nav.php'; ?>
 		</div>
-		<div class="white-slice top-slice">
-            <div class="seveneighty">
-                <h2 class="purple">Introducing Mirrors &amp; Windows</h2>
-                <p>EMC School is proud to present its exciting literature and language arts program, Mirrors &amp; Windows. 
-				This seven-level program is built on a collection of rich, diverse, and timeless writings by renowned, award-winning authors.
-				</p>
-
-				<p>Mirrors &amp; Windows challenges students to reach their maximum potential while differentiating instruction for individual learners. 
-				Using a gradual release approach to reading, students will achieve a deep comprehension of the material and a greater appreciation of the 
-				literary genres. They will learn to recognize and make connections between the selections, the world, and themselves. Along the way, they 
+		<div class="secondary-first">
+			<div class="ninesixty">
+                <h2 class="purple" style="display:inline !important;">INTRODUCING</h2> <span class="mwtitle">Mirrors &amp; Windows</span>
+                <p style="line-height:1.6;"><img src="/lib/images/texasela-hero.png" style="float:right;margin-left:20px;" />
+				<strong>EMC School</strong> is proud to present its exciting literature and language arts program, <span class="italic">Mirrors &amp; Windows</span>. 
+				This seven-level program is built on a collection of rich, diverse, and timeless writings by renowned, award-winning authors.<br /><br />
+				
+				<span class="italic">Mirrors &amp; Windows</span> <strong>challenges students</strong> to reach their maximum potential while differentiating instruction for individual learners. 
+				Using a <strong>gradual release approach to reading</strong>, students will achieve a deep comprehension of the material and a greater appreciation of the 
+				literary genres. They will learn to <strong>recognize and make connections</strong> between the selections, the world, and themselves. Along the way, they 
 				will discover a love of literature that will grow throughout their lives. Truly, literature will turn mirrors into windows for your students.
 				</p>
             </div>
+			<div class="clearfix"></div>
         </div>
-		<div class="grey-slice paddingtop20 paddingbottom40">
-            <div class="seveneighty">
-                <p> Among its extensive supplemental materials a variety of Texas authors including:</p>
-				<strong>Level I/Grade 6</strong>
-				<ul>
-					<li><strong>Pat Mora</strong> – author of "Same Song" and "Gold" – from El, Paso, TX</li>
-					<li><strong>Robert Fulghum</strong> – author of "All I Really Need to Know I Learned in Kindergarten" – from Waco, TX</li>
-					<li><strong>Carmen Tafolla</strong> – author of "Mi Familia" – from San Antonio, TX</li>
-				</ul>
-				<strong>Level II/Grade 7</strong>
-				<ul>
-					<li><strong>Tomas Rivera</strong> – author of "The Portrait" – from Crystal City, TX (passed away in 1984)</li>
-					<li><strong>Naomi Shihab Nye</strong> – author of "The Lost Parrot" –lives in San Antonio, TX</li>
-				</ul>
-				<strong>Level III/Grade 8</strong>
-				<ul>
-					<li><strong>Angela Shelf Medearis</strong> – author of "I Was Born at the Wrong Time" –lives in Austin, TX</li>
-					<li><strong>Roberto Felix Salazar</strong> – author of "The Other Pioneers" –from Laredo, TX</li>
-				</ul>
+		<div class="redefining no-bottom light-grey-slice">
+            <div class="ninesixty paddingtop40 paddingbottom40">
+                <p><strong>Among its extensive supplemental materials a variety of Texas authors including:</strong></p>
+				<div class="one-third">
+					<span style="font-weight:bold;color:#ff0000;">Level I/Grade 6</span>
+					<ul style="margin-top:0;line-height:1.2;">
+						<li><strong>Pat Mora</strong> – author of "Same Song" and "Gold" – from El, Paso, TX</li>
+						<li><strong>Robert Fulghum</strong> – author of "All I Really Need to Know I Learned in Kindergarten" – from Waco, TX</li>
+						<li><strong>Carmen Tafolla</strong> – author of "Mi Familia" – from San Antonio, TX</li>
+					</ul>
+				</div>
+				<div class="one-third">
+					<span style="font-weight:bold;color:#ff0000;">Level II/Grade 7</span>
+					<ul style="margin-top:0;line-height:1.2;">
+						<li><strong>Tomas Rivera</strong> – author of "The Portrait" – from Crystal City, TX (passed away in 1984)</li>
+						<li><strong>Naomi Shihab Nye</strong> – author of "The Lost Parrot" –lives in San Antonio, TX</li>
+					</ul>
+				</div>
+				<div class="one-third">
+					<span style="font-weight:bold;color:#ff0000;">Level III/Grade 8</span>
+					<ul style="margin-top:0;line-height:1.2;">
+						<li><strong>Angela Shelf Medearis</strong> – author of "I Was Born at the Wrong Time" –lives in Austin, TX</li>
+						<li><strong>Roberto Felix Salazar</strong> – author of "The Other Pioneers" –from Laredo, TX</li>
+					</ul>
+				</div>
+				<div class="clearfix"></div>
             </div>
         </div>
-		<div class="white-slice paddingtop20 paddingbottom20">
-            <div class="seveneighty">
-                <h2 class="purple">It's here!</h2>
-                <p>EMC School's English Language Arts program Mirrors &amp; Windows has arrived on our state-of-the-art learning platform Passport&reg;</p>
+		<div class="redefining paddingtop40">
+            <div class="ninesixty">
+				<div class="tx-left-block">
+					<div style="margin-top:40px;"><img src="/lib/images/texasela-passport-logo.png" style="margin:0 !important;" /></div>
+					<h1 style="font-size:72px !important;text-align:center;"><span class="italic">IT'S HERE!</span></h1>
+					<p style="line-height:1.6;text-align:center;">EMC School's English Language Arts program Mirrors &amp; Windows has arrived on our state-of-the-art learning platform Passport&reg;</p>
+				</div>
+				<div class="tx-left-block">
+					<img src="/lib/images/texasela-passport-ipad.png" style="float:left;margin-left:20px;" />
+				</div>
+				<div class="clearfix"></div>
+				<div>
+					<p style="line-height:1.6;">Every language arts publisher can say they have great literature, but only <strong>EMC School has Passport&reg;</strong></p>
+				</div>
+				<div><p>Built from the ground up, EMC School has uniquely designed Passport&reg; for your language arts classroom. It’s built to be intuitive and 
+					extremely user-friendly while the social media look and feel instantly clicks with students. Easy-to-use, Passport&reg; helps students focus 
+					more on the content while the discussion tools engage and help them to dive deeper into the curriculum and connect with fellow classmates.</p>
 
-				<p>Every language arts publisher can say they have great literature, but only <strong>EMC School has Passport&reg;</strong><br />
-				Built from the ground up, EMC School has uniquely designed Passport&reg; for your language arts classroom. It’s built to be intuitive and 
-				extremely user-friendly while the social media look and feel instantly clicks with students. Easy-to-use, Passport&reg; helps students focus 
-				more on the content while the discussion tools engage and help them to dive deeper into the curriculum and connect with fellow classmates.</p>
-
-				<p>Passport&reg; is real-world! The ELA curriculum included teaches students useful skills to prepare them for college and future careers.<br />
-				For teaching professionals: Mirrors &amp; Windows with Passport&reg; allows teachers easy access to all program resources. It provides a way to 
-				track the progress of all students visually with stunning graphics and charts. It also gives teachers a straightforward way to assign homework 
-				and tasks as well as provide feedback to students.
-				</p>
+					<p>Passport&reg; is real-world! The ELA curriculum included teaches students useful skills to prepare them for college and future careers.<br />
+					For teaching professionals: Mirrors &amp; Windows with Passport&reg; allows teachers easy access to all program resources. It provides a way to 
+					track the progress of all students visually with stunning graphics and charts. It also gives teachers a straightforward way to assign homework 
+					and tasks as well as provide feedback to students.
+					</p>
+				</div>
+			</div>
+        </div>
+		<div class="mw-mp-ebook paddingtop40 paddingbottom40 light-grey-slice">
+			<div class="ninesixty">
+                <div style="padding:40px 10px;border:3px solid #f00;border-radius:10px;background-image: url('/lib/images/texasela-state.png');background-repeat:no-repeat;background-position:left center;">
+					<div style="margin:0 50px 25px 300px;clear:both;">
+						<div><strong>These Texas districts have already discovered the power of Passport&reg;</strong></div>
+						<div class="clearfix"></div>
+						<div>
+							<div style="float:left;line-height:1.1 !important;">
+								<ul style="line-height:1.1 !important;font-size:15px;">
+									<li>Houston ISD</li>
+									<li>Deer Park ISD</li>
+									<li>Alief ISD</li>
+									<li>Klein ISD</li>
+									<li>Eanes ISD</li>
+									<li>Round Rock ISD</li>
+								</ul>
+							</div>
+							<div style="float:left;">
+								<ul style="line-height:1.1 !important;font-size:15px;">
+									<li>Leander ISD</li>
+									<li>Judson ISD</li>
+									<li>North East ISD</li>
+									<li>Corpus Christi ISD</li>
+									<li>Irving ISD</li>
+									<li>Birdville ISD</li>
+								</ul>
+							</div>
+							<div style="float:left;">
+								<ul style="line-height:1.1 !important;font-size:15px;">
+									<li>Coppell ISD</li>
+									<li>Garland ISD</li>
+									<li>Mesquite ISD</li>
+									<li>Edinburg ISD</li>
+									<li>Pharr-San Juan-Alamo ISD</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div style="text-align:center;clear:both;">and many more!</div>
+				</div>
             </div>
+			<div class="clearfix"></div>
         </div>
     </div>
 <?php include '../includes/footer.php'; ?>
