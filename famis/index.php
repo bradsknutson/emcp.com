b0157b043e7f4ff17cc17cc7f3d2797e86004df4
Brian Molina <bmolina@emcp.com>
2018-02-20 09:44:47 -0700

    Updates to FAMIS page
    Updates to FAMIS page

--- famis/index.php
+++ famis/index.php
@@ -12,7 +12,10 @@
 		<div class="white-slice light-grey-slice al-top-content">
 			<div class="ninesixty">
                 <div>
-					<p class="lineheight1p75" style="margin:0;">Thanks for your interest in EMC's 2016 FAMIS catalog. Scroll down to view, download or share our New York catalog. Questions? Please feel free to contact your <a href="http://store.emcp.com/about-emcp/local-account-manager-locator">Account Manager</a>.</p>
+					<p class="lineheight1p75" style="margin:0;">
+						Thanks for your interest in EMC's 2018 FAMIS catalog. Scroll down to view, download or share our New York catalog. 
+						Questions? Please feel free to contact your <a href="http://store.emcp.com/about-emcp/local-account-manager-locator">Account Manager</a>.
+					</p>
 				</div>
                 <div class="clearfix"></div>
             </div>
@@ -20,9 +23,16 @@
         <div class="white-slice">
 			<div class="ninesixty">
                 <div class="catalog-img">
-                    <a href="https://drive.google.com/file/d/0B2wIWFpktdtATFcyT1JfNHpldkU/view?usp=sharing" target="_blank">
-                        <img src="<?php echo $server; ?>lib/images/famis-catalog-cover.jpg" alt="World Languages Catalog" class="animated" />
-                    </a>
+                    <a href="https://drive.google.com/file/d/1kkkb1wXWah8zwKnAI9t5foEiu2aAH1cs/view?usp=sharing" target="_blank">
+                        <img src="<?php echo $server; ?>lib/images/famis-catalog-cover-2018.jpg" alt="FAMIS 2018 Catalog" class="animated" />
+                    </a><br /><br />
+					<strong>FAMIS Catalog</strong>
+                </div>
+				<div class="catalog-img">
+                    <a href="https://drive.google.com/file/d/1V38NQwlgXMwSFtwY2K_f5H3YmT60gjeZ/view?usp=sharing" target="_blank">
+                        <img src="<?php echo $server; ?>lib/images/famis-catalog-pricing-2018.jpg" alt="FAMIS 2018 Price List" class="animated" />
+                    </a><br /><br />
+					<strong>Pricing information</strong>
                 </div>
                 <div class="clearfix"></div>
             </div>            
