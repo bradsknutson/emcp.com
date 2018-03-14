e38b4656ac07196adcf997445e9cf6732dd00e9f
Brian Molina <bmolina@emcp.com>
2018-03-14 12:31:22 -0600

    Texas Resources Page Images
    Texas Resources Page Images

--- texasela/resources.php
+++ texasela/resources.php
@@ -15,10 +15,9 @@
 		<div class="secondary-first">
 			<div class="ninesixty" style="padding:20px 0 10px;">
                <h1 style="text-align:center;">Mirrors &amp; Windows Resources</h1>
-                <p style="line-height:1.6;">
-					Hello, Texas Teachers. Welcome to our Proclamation 2020 Resources site. EMC offers its ©2020 Mirrors &amp; Windows English Language Arts Program 
+                <h2>Hello, Texas Teachers. Welcome to our Proclamation 2020 Resources site. EMC offers its ©2020 Mirrors &amp; Windows English Language Arts Program 
 					supported by our award winning digital platform Passport<sup>&reg;</sup>. 
-				</p>
+				</h2>
             </div>
 			<div class="clearfix"></div>
         </div>
@@ -43,6 +42,22 @@
 		<div class="redefining no-bottom light-grey-slice">
             <div class="ninesixty paddingtop40 paddingbottom40">
 				<div class="one-half first">
+					<h2>Meet Passport<sup>&reg;</sup></h2>			
+					<p style="line-height:1.6;">
+						Passport, our award winning digital platform, is the foundation of our Mirrors &amp; Windows English Language Arts Program<br /><br />
+					</p>
+					<p style="line-height:1.6;"><a href="https://joom.ag/OR1Y" target="_blank" class="learn-more-link">View now</a> >></p>
+				</div>
+				<div class="one-half last">
+					<img src="/lib/images/texas-ela-resources-passport.png" style="margin:0 !important;" />
+				</div>
+				<div class="clearfix"></div>
+            </div>
+        </div>
+	<!-- Slice 3 -->
+		<div class="redefining no-bottom">
+            <div class="ninesixty paddingtop40 paddingbottom40">
+				<div class="one-half first">
 					<h2>Sample Lesson</h2>
 					<p style="line-height:1.6;">
 						Middle school Mirrors &amp; Windows English Language Arts Program<br /><br />
@@ -55,8 +70,8 @@
 				<div class="clearfix"></div>
             </div>
         </div>
-	<!-- Slice 3 -->
-		<div class="redefining no-bottom">
+	<!-- Slice 4 -->
+		<div class="redefining no-bottom light-grey-slice">
             <div class="ninesixty paddingtop40 paddingbottom40">
 				<div class="one-half first">
 					<h2>List of Authors</h2>		
@@ -71,8 +86,8 @@
 				<div class="clearfix"></div>
             </div>
         </div>
-	<!-- Slice 4 -->
-		<div class="redefining no-bottom light-grey-slice">
+	<!-- Slice 5 -->
+		<div class="redefining no-bottom">
             <div class="ninesixty paddingtop40 paddingbottom40">
 				<div class="one-half first">
 					<h2>ELPS Support</h2>			
@@ -87,8 +102,8 @@
 				<div class="clearfix"></div>
             </div>
         </div>
-	<!-- Slice 5 -->
-		<div class="redefining no-bottom">
+	<!-- Slice 6 -->
+		<div class="redefining no-bottom light-grey-slice">
             <div class="ninesixty paddingtop40 paddingbottom40">
 				<div class="one-half first">
 					<h2>Program Overview Brochure</h2>			
@@ -103,8 +118,8 @@
 				<div class="clearfix"></div>
             </div>
         </div>
-	<!-- Slice 6 -->
-		<div class="redefining no-bottom light-grey-slice">
+	<!-- Slice 7 -->
+		<div class="redefining no-bottom">
             <div class="ninesixty paddingtop40 paddingbottom40">
 				<div class="one-half first">
 					<h2>Spanish Readers</h2>			
@@ -115,22 +130,6 @@
 				</div>
 				<div class="one-half last">
 					<img src="/lib/images/texas-ela-resources-spanish-readers.png" style="margin:0 !important;" />
-				</div>
-				<div class="clearfix"></div>
-            </div>
-        </div>
-	<!-- Slice 7 -->
-		<div class="redefining no-bottom">
-            <div class="ninesixty paddingtop40 paddingbottom40">
-				<div class="one-half first">
-					<h2>Passport<sup>&reg;</sup> Brochure</h2>			
-					<p style="line-height:1.6;">
-						Passport, our award winning digital platform, is the foundation of our Mirrors &amp; Windows English Language Arts Program<br /><br />
-					</p>
-					<p style="line-height:1.6;"><a href="https://joom.ag/OR1Y" target="_blank" class="learn-more-link">View now</a> >></p>
-				</div>
-				<div class="one-half last">
-					<img src="/lib/images/texas-ela-resources-passport.png" style="margin:0 !important;" />
 				</div>
 				<div class="clearfix"></div>
             </div>
