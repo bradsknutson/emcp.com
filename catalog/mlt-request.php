f1a48bc80a277a93231ae1086b72fe44cf493488
Brian Molina <bmolina@emcp.com>
2018-04-02 10:45:45 -0600

    Add MLT catalog request page

---
+++ catalog/mlt-request.php
@@ -0,0 +1,16 @@
+ <?php
+	$custom_title = 'Request Form | ';
+	include '../includes/header.php';
+?>
+	
+	<div class="header-secondary-page"></div>
+	<div class="header-top-dark">
+		<?php include '../includes/nav.php'; ?>
+	</div>
+	<div class="ninesixty">
+		<div style="text-align:center;margin-top:25px;"><img src="<?php echo $server; ?>lib/images/mlt-logo.jpg" alt="Mmy Language Town - Catalog Request" /></div>
+		<iframe src="https://pages.exacttarget.com/emcmltsample" class="form" style="height:900px;width:100%;border:0;"></iframe>
+		<div class="clearfix"></div>
+	</div>
+
+<?php include '../includes/footer.php'; ?>
