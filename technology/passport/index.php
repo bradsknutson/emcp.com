c9245f1f5166c8ffe9b7313d152da0c72d147a2f
Brian Molina <bmolina@emcp.com>
2018-04-16 21:19:46 +0200

    Passport landing page updates
    Passport landing page updates

--- technology/passport/index.php
+++ technology/passport/index.php
@@ -21,9 +21,9 @@
             </div>
         </div>
         <div class="fofih">
-            <div class="ninehundred">
+            <div class="ninehundred" style="padding-bottom:10px;">
                 <h2 class="textcenter"><br />The learning environment for the world language classroom of tomorrow&mdash;today</h2>
-                <div class="left-block">
+                <div class="left-block" style="width:50%;">
                     <p>Passport<sup>&reg;</sup> is an innovative world language learning environment that allows teachers to easily assign classwork, track student progress, and report results to parents or school districts – all within a single application.</p>
 
                     <p>Passport<sup>&reg;</sup> seamlessly delivers EMC's leading world language content and assets in a research-based platform developed by the Institute for Design Innovation at the University of Minnesota.</p>
@@ -34,91 +34,85 @@
                     <img src="<?php echo $server; ?>lib/images/passport-landing-1.jpg" />
                 </div>
                 <div class="clearfix"></div>
-                <div class="left-block">
+			</div>
+		</div>
+		<div>
+			 <div class="ninehundred" style="padding-top:0;">
+                <div class="left-block" style="width:50%;">
                     <img src="<?php echo $server; ?>lib/images/passport-landing-2.jpg" />
                 </div>
-                <div class="right-block">
-                    <p class="no-bottom paddingtop40">Passport allows students to keep discovering world languages online through</p>
-                    <ul>
+                <div class="right-block" style="width:50%;">
+                    <p class="no-bottom paddingtop40" style="font-size:23px;line-height:1.4;">Passport allows students to keep discovering world languages online through</p>
+                    <ul style="font-size:23px;line-height:1.4;">
                         <li>real-life communication</li>
                         <li>cultural immersion</li>
                         <li>performance-based tasks</li>
                         <li>project-based learning</li>
                     </ul>
-                    <h3 class="no-bottom">Access to learning anytime, anywhere</h3>
-                    <p class="no-top">All content available on desktop computers, laptops, tablets, or smartphones.</p>
+                    <p class="no-top" style="font-size:23px;line-height:1.4;">Access to learning anytime, anywhere<br />
+                    All content available on desktop computers, laptops, tablets, or smartphones.
+					</p>
                 </div>
                 <div class="clearfix"></div>
             </div>
         </div>
-        <div class="grey-slice">
-            <div class="ninehundred">
-                <div class="left-block">
-                    <h3 class="paddingtop40 no-bottom">Easy reporting</h3>
-                    <p class="no-top">Report your students’ progress on activities, assessments, and standards throughout the year with customizable, easy-to-export graphs and charts.</p>
+	<!-- New grey slice -->
+		<div class="grey-slice" style="padding:10px 0 20px !important;">
+            <div class="ninehundred" style="padding:0 !important;">
+				<div style="margin:10px 0;text-align:center;"><h2 class="no-bottom">Get to Know Passport<sup>&reg;</sup> Weekly Webinar Series</h2></div>
+				<div class="clearfix"></div>
+                <div style="text-align:center;">
+                    <img src="<?php echo $server; ?>lib/images/PassportWebinar_700px.gif" style="margin-top:10px;" /><br />
                 </div>
-                <div class="right-block">
-                    <img src="<?php echo $server; ?>lib/images/passport-landing-3.jpg" />
+				<div class="clearfix"></div>
+                <div style="text-align:center;">
+                    <p class="no-top" style="line-height:1.4;"><br />
+						Get the most out of your demo by getting to know the engaging<br /> 
+						Passport<sup>&reg;</sup> digital environment for middle and high school learners!
+					</p>
+					<p style="text-align:center;"><a href="https://calendly.com/emcschool/passportwebinarwl/" style="background:#FF7F27;padding:10px;border-radius:5px;color:#fff;font-size:23px;" target="_blank">Sign up for a webinar</a>
                 </div>
                 <div class="clearfix"></div>
             </div>
         </div>
         <div class="passport-third-section">
             <div class="ninesixty paddingtop40 paddingbottom40">
-                <div class="left-block">
+                <div class="left-block" style="width:50%;">
                     <img src="<?php echo $server; ?>lib/images/passport-landing-4.jpg" />
                 </div>
-                <div class="right-block">
+                <div class="right-block" style="width:50%;">
                     <h3 class="paddingtop20">Keep discovering</h3>
                     <div class="passport-icons-container">
-                        <div class="icons-left">
-                            <img src="<?php echo $server; ?>lib/images/passport-icons-assess.png" alt="Passport - Assess" />
-                        </div>
-                        <div class="icons-right">
-                            <p style="line-height:1.1;">Assess &mdash; Students take formative surveys and selection and unit chapter tests online.</p>
-                        </div>
-                        <div class="clearfix"></div>
-                        <div class="icons-left">
-                            <img src="<?php echo $server; ?>lib/images/passport-icons-discover.png" alt="Passport - Discover" />                        
-                        </div>
-                        <div class="icons-right">
-                            <p style="line-height:1.1;">Discover &mdash; Students connect to core curriculum via their eBooks and other supplemental materials.</p>
-                        </div>
-                        <div class="clearfix"></div>
-                        <div class="icons-left">
-                            <img src="<?php echo $server; ?>lib/images/passport-icons-expand.png" alt="Passport - Expand" /> 
-                        </div>
-                        <div class="icons-right">
-                            <p style="line-height:1.1;">Expand &mdash; Students explore close reading and media literacy skills in 42+ novels with new novels added regularly.</p>
-                        </div>
-                        <div class="clearfix"></div>
-                        <div class="icons-left">
-                            <img src="<?php echo $server; ?>lib/images/passport-icons-create.png" alt="Passport - Create" /> 
-                        </div>
-                        <div class="icons-right">
-                            <p style="line-height:1.1;">Create &mdash; Students practice reading, writing, speaking and listening, and language skills through project-based learning.</p>
-                        </div>
-                        <div class="clearfix"></div>
-                        <div class="icons-left">
-                            <img src="<?php echo $server; ?>lib/images/passport-icons-explore.png" alt="Passport - Explore" /> 
-                        </div>
-                        <div class="icons-right">
-                            <p style="line-height:1.1;">Explore &mdash; Students learn the close reading process through interactive videos.</p>
-                        </div>
-                        <div class="clearfix"></div>
-						 <div class="icons-left">
-                            <img src="<?php echo $server; ?>lib/images/passport-icons-share.png" alt="Passport - Share" /> 
-                        </div>
-                        <div class="icons-right">
-                            <p style="line-height:1.1;">Share &mdash; Students post their opinions, ideas, stories, and more, in a social media format using Flipgrid.</p>
-                        </div>
-                        <div class="clearfix"></div>
-						 <div class="icons-left">
-                            <img src="<?php echo $server; ?>lib/images/passport-icons-perform.png" alt="Passport - Perform" /> 
-                        </div>
-                        <div class="icons-right">
-                            <p style="line-height:1.1;">Perform &mdash; Students demonstrate oral proficiency through video assessment tasks and can create a video performance portfolio.</p>
-                        </div>
+                        <table width="100%" border="0" cellpadding="0" cellspacing="0">
+							<tr>
+								<td width="6%" align="center" valign="top" style="border-style:none;"><img src="<?php echo $server; ?>lib/images/passport-icons-assess.png" alt="Passport - Assess" /></td>
+								<td width="94%" valign="top" style="border-style:none;"><p style="line-height:1.1;font-size:20px;padding:0 0 0 10px;margin:0 0 5px;">Assess &mdash; Students take formative surveys and selection and unit chapter tests online.</p></td>
+							</tr>
+							<tr>
+								<td width="6%" align="center" valign="top" style="border-style:none;"><img src="<?php echo $server; ?>lib/images/passport-icons-discover.png" alt="Passport - Discover" /></td>
+								<td width="94%" valign="top" style="border-style:none;"><p style="line-height:1.1;font-size:20px;padding:0 0 0 10px;margin:0 0 5px;">Discover &mdash; Students connect to core curriculum via their eBooks and other supplemental materials.</p></td>
+							</tr>
+							<tr>
+								<td width="6%" align="center" valign="top" style="border-style:none;"><img src="<?php echo $server; ?>lib/images/passport-icons-expand.png" alt="Passport - Expand" /></td>
+								<td width="94%" valign="top" style="border-style:none;"><p style="line-height:1.1;font-size:20px;padding:0 0 0 10px;margin:0 0 5px;">Expand &mdash; Students explore close reading and media literacy skills in 42+ novels with new novels added regularly.</p></td>
+							</tr>
+							<tr>
+								<td width="6%" align="center" valign="top" style="border-style:none;"><img src="<?php echo $server; ?>lib/images/passport-icons-create.png" alt="Passport - Create" /> </td>
+								<td width="94%" valign="top" style="border-style:none;"><p style="line-height:1.1;font-size:20px;padding:0 0 0 10px;margin:0 0 5px;">Create &mdash; Students practice reading, writing, speaking and listening, and language skills through project-based learning.</p></td>
+							</tr>
+							<tr>
+								<td width="6%" align="center" valign="top" style="border-style:none;"><img src="<?php echo $server; ?>lib/images/passport-icons-explore.png" alt="Passport - Explore" /></td>
+								<td width="94%" valign="top" style="border-style:none;"><p style="line-height:1.1;font-size:20px;padding:0 0 0 10px;margin:0 0 5px;">Explore &mdash; Students learn the close reading process through interactive videos.</p></td>
+							</tr>
+							<tr>
+								<td width="6%" align="center" valign="top" style="border-style:none;"><img src="<?php echo $server; ?>lib/images/passport-icons-share.png" alt="Passport - Share" /></td>
+								<td width="94%" valign="top" style="border-style:none;"><p style="line-height:1.1;font-size:20px;padding:0 0 0 10px;margin:0 0 5px;">Share &mdash; Students post their opinions, ideas, stories, and more, in a social media format using Flipgrid.</p></td>
+							</tr>
+							<tr>
+								<td width="6%" align="center" valign="top" style="border-style:none;"><img src="<?php echo $server; ?>lib/images/passport-icons-perform.png" alt="Passport - Perform" /></td>
+								<td width="94%" valign="top" style="border-style:none;"><p style="line-height:1.1;font-size:20px;padding:0 0 0 10px;margin:0 0 5px;">Perform &mdash; Students demonstrate oral proficiency through video assessment tasks and can create a video performance portfolio.</p></td>
+							</tr>
+						</table>
                         <div class="clearfix"></div>
                     </div>
 					<div>
@@ -136,6 +130,18 @@
 							<li>custom lesson creation 
 						</ul>
 					</div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+		<div class="grey-slice" style="padding:10px 0 20px !important;">
+            <div class="ninehundred" style="padding:0 !important;">
+                <div class="left-block">
+                    <h2 class="no-bottom">Easy reporting</h2>
+                    <p class="no-top">Report your students’ progress on activities, assessments, and standards throughout the year with customizable, easy-to-export graphs and charts.</p>
+                </div>
+                <div class="right-block">
+                    <img src="<?php echo $server; ?>lib/images/passport-landing-3.jpg" />
                 </div>
                 <div class="clearfix"></div>
             </div>
