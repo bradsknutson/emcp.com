eb77ad1d514344eac990a5f17f077cd329bf230b
Brian Molina <bmolina@emcp.com>
2018-05-11 12:57:29 -0700

    Formatting links

--- texas/resources/index.php
+++ texas/resources/index.php
@@ -46,28 +46,28 @@
 				<h3>TEKS for Computer Science</h3>
 				<strong>Core Content:</strong>
 				<ul>
-					<li><a href="https://drive.google.com/file/d/1UxkwZ3lAqA0ZDEZOWTnI0CtoiU9gXbrH/view?usp=sharing" target="_blank">126.32 - Fund of CS TEKS Correlation Intro Comp Sci</a></li>
-					<li><a href="https://drive.google.com/file/d/1v3KLZOgyl0alX9-T-8mdj92r45nINinD/view?usp=sharing" target="_blank">126.33 - Comp Sci I TEKS Correlation GameMakerProg I</a></li>
-					<li><a href="https://drive.google.com/file/d/18Z7UqebPUWQbFaZBmZ6v7UXsqXVaQgso/view?usp=sharing" target="_blank">126.38 - Game Prog Design TEKS Correlation Coding with GameMaker</a></li>
-					<li><a href="https://drive.google.com/file/d/1yM-az4ADpFTjT_MbRLJvKwyxwT3WUK5G/view?usp=sharing" target="_blank">126.38 - Game Prog Design TEKS Correlation GameMakerProg I</a></li>
-					<li><a href="https://drive.google.com/file/d/1ZqMvNQldF0rbv-MXAO7xpcm6jdTG9jRx/view?usp=sharing" target="_blank">126.38 - Game Prog Design TEKS Correlation GameMakerProg II</a></li>
-					<li><a href="https://drive.google.com/file/d/1oesh42RnWYrqf_3_Iw7pOra5ugngsi6F/view?usp=sharing" target="_blank">126.38 - Game Prog Design TEKS Correlation Intro Comp Sci Game Design</a></li>
-					<li><a href="https://drive.google.com/file/d/1ol6OoZARVJeFJT8PbBp7isux7nt8TKDa/view?usp=sharing" target="_blank">126.43 3-D Modeling and Animation TEKS Correlation 3D Modeling</a></li>
-					<li><a href="https://drive.google.com/file/d/1e2q6KpcF7Uyi215GGy6xQOmOXIZaf9uD/view?usp=sharing" target="_blank">126.44 Digital Com in the 21st Cen TEKS Correlation RealWorldProjects</a></li>
-					<li><a href="https://drive.google.com/file/d/1l7ObNpbPE0lhciTWEO8CyuVPeSd2eL3k/view?usp=sharing" target="_blank">126.49 Indep Study in Technology Apps TEKS Correlation Real World Projects</a></li>
-					<li><a href="https://drive.google.com/file/d/1EKcEGKfp9Sup5UycbH-GkbjXMFVU3NlE/view?usp=sharing" target="_blank">126.50 Indep Study in Emerging Tech TEKS Correlation Real World Projects</a></li>
+					<li><a href="https://drive.google.com/file/d/1UxkwZ3lAqA0ZDEZOWTnI0CtoiU9gXbrH/view?usp=sharing" target="_blank" style="color:#C10005";>126.32 - Fund of CS TEKS Correlation Intro Comp Sci</a></li>
+					<li><a href="https://drive.google.com/file/d/1v3KLZOgyl0alX9-T-8mdj92r45nINinD/view?usp=sharing" target="_blank" style="color:#C10005";>126.33 - Comp Sci I TEKS Correlation GameMakerProg I</a></li>
+					<li><a href="https://drive.google.com/file/d/18Z7UqebPUWQbFaZBmZ6v7UXsqXVaQgso/view?usp=sharing" target="_blank" style="color:#C10005";>126.38 - Game Prog Design TEKS Correlation Coding with GameMaker</a></li>
+					<li><a href="https://drive.google.com/file/d/1yM-az4ADpFTjT_MbRLJvKwyxwT3WUK5G/view?usp=sharing" target="_blank" style="color:#C10005";>126.38 - Game Prog Design TEKS Correlation GameMakerProg I</a></li>
+					<li><a href="https://drive.google.com/file/d/1ZqMvNQldF0rbv-MXAO7xpcm6jdTG9jRx/view?usp=sharing" target="_blank" style="color:#C10005";>126.38 - Game Prog Design TEKS Correlation GameMakerProg II</a></li>
+					<li><a href="https://drive.google.com/file/d/1oesh42RnWYrqf_3_Iw7pOra5ugngsi6F/view?usp=sharing" target="_blank" style="color:#C10005";>126.38 - Game Prog Design TEKS Correlation Intro Comp Sci Game Design</a></li>
+					<li><a href="https://drive.google.com/file/d/1ol6OoZARVJeFJT8PbBp7isux7nt8TKDa/view?usp=sharing" target="_blank" style="color:#C10005";>126.43 - 3-D Modeling and Animation TEKS Correlation 3D Modeling</a></li>
+					<li><a href="https://drive.google.com/file/d/1e2q6KpcF7Uyi215GGy6xQOmOXIZaf9uD/view?usp=sharing" target="_blank" style="color:#C10005";>126.44 - Digital Com in the 21st Cen TEKS Correlation RealWorldProjects</a></li>
+					<li><a href="https://drive.google.com/file/d/1l7ObNpbPE0lhciTWEO8CyuVPeSd2eL3k/view?usp=sharing" target="_blank" style="color:#C10005";>126.49 - Indep Study in Technology Apps TEKS Correlation Real World Projects</a></li>
+					<li><a href="https://drive.google.com/file/d/1EKcEGKfp9Sup5UycbH-GkbjXMFVU3NlE/view?usp=sharing" target="_blank" style="color:#C10005";>126.50 - Indep Study in Emerging Tech TEKS Correlation Real World Projects</a></li>
 				</ul>
 
 				<strong>Supplementary Content:</strong>
 				<ul>
-					<li><a href="https://drive.google.com/file/d/1ytvJaP1oKolapYsmeDSWCaNkL3rgWtjD/view?usp=sharing" target="_blank">126.32 Fund of CS TEKS Correlation Coding with GameMaker</a></li>
-					<li><a href="https://drive.google.com/file/d/1uXgAAsPt13RdYQ2WY3V0dsuwNb-V3GLF/view?usp=sharing" target="_blank">126.33 Comp Sci1 TEKS Correlation Coding with GameMaker</a></li>
-					<li><a href="https://drive.google.com/file/d/15o0H8tHHOTOAv5bYnAgy4uP1zlXnc4nY/view?usp=sharing" target="_blank">126.34 Comp Sci II TEKS Correlation GameMakerProg II</a></li>
-					<li><a href="https://drive.google.com/file/d/1OMAi6obis1I7_A_kdCfINqD-0dwqXyDN/view?usp=sharing" target="_blank">126.35 Comp Sci III TEKS Correlation Unity 3D</a></li>
-					<li><a href="https://drive.google.com/file/d/1OCERARxdI8xu-DdGjhaF3wpIACdrNPAN/view?usp=sharing" target="_blank">126.40 Robotics Pgm Design TEKS Correlation Arcade Game Design</a></li>
-					<li><a href="https://drive.google.com/file/d/1FXrkjyDh47PAMCCWzC5tiXpWdPIBd7cc/view?usp=sharing" target="_blank">126.42 Digital Art and Animation TEKS Correlation Making Mobile Games</a></li>
-					<li><a href="https://drive.google.com/file/d/1N9YoZ_L_BtkqkNys9HWyRA5iOp2frrUr/view?usp=sharing" target="_blank">126.42 Digital Art and Animation TEKS Correlation Mobile Game Design</a></li>
-					<li><a href="https://drive.google.com/file/d/1vsMrDM01QJ5Xu8oKBv-Foo9xOlZr6TMS/view?usp=sharing" target="_blank">126.45 Digital Video and Audio Design TEKS Correlation Screenwriting</a></li>
+					<li><a href="https://drive.google.com/file/d/1ytvJaP1oKolapYsmeDSWCaNkL3rgWtjD/view?usp=sharing" target="_blank" style="color:#C10005";>126.32 - Fund of CS TEKS Correlation Coding with GameMaker</a></li>
+					<li><a href="https://drive.google.com/file/d/1uXgAAsPt13RdYQ2WY3V0dsuwNb-V3GLF/view?usp=sharing" target="_blank" style="color:#C10005";>126.33 - Comp Sci1 TEKS Correlation Coding with GameMaker</a></li>
+					<li><a href="https://drive.google.com/file/d/15o0H8tHHOTOAv5bYnAgy4uP1zlXnc4nY/view?usp=sharing" target="_blank" style="color:#C10005";>126.34 - Comp Sci II TEKS Correlation GameMakerProg II</a></li>
+					<li><a href="https://drive.google.com/file/d/1OMAi6obis1I7_A_kdCfINqD-0dwqXyDN/view?usp=sharing" target="_blank" style="color:#C10005";>126.35 - Comp Sci III TEKS Correlation Unity 3D</a></li>
+					<li><a href="https://drive.google.com/file/d/1OCERARxdI8xu-DdGjhaF3wpIACdrNPAN/view?usp=sharing" target="_blank" style="color:#C10005";>126.40 - Robotics Pgm Design TEKS Correlation Arcade Game Design</a></li>
+					<li><a href="https://drive.google.com/file/d/1FXrkjyDh47PAMCCWzC5tiXpWdPIBd7cc/view?usp=sharing" target="_blank" style="color:#C10005";>126.42 - Digital Art and Animation TEKS Correlation Making Mobile Games</a></li>
+					<li><a href="https://drive.google.com/file/d/1N9YoZ_L_BtkqkNys9HWyRA5iOp2frrUr/view?usp=sharing" target="_blank" style="color:#C10005";>126.42 - Digital Art and Animation TEKS Correlation Mobile Game Design</a></li>
+					<li><a href="https://drive.google.com/file/d/1vsMrDM01QJ5Xu8oKBv-Foo9xOlZr6TMS/view?usp=sharing" target="_blank" style="color:#C10005";>126.45 - Digital Video and Audio Design TEKS Correlation Screenwriting</a></li>
 				</ul>
             </div>
         </div>
