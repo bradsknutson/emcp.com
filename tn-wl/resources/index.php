3b7261450587794ab53884e1aa22ab790cda4aef
Brian Molina <bmolina@emcp.com>
2018-10-01 14:46:15 -0600

    TN Landing pages

---
+++ tn-wl/resources/index.php
@@ -0,0 +1,59 @@
+<?php 
+	$custom_title = 'Tennessee Resources | Proclamation 2018 | ';
+	include '../../includes/header.php';
+?>
+	<div id="container" class="tn-landing">
+		<div class="header-background-image texas-resources-bg">
+			<div id="topImg">
+				<!--
+				<div style="padding-top:525px;"><a class="mw-buttons" href="http://pages.exacttarget.com/page.aspx?QS=38dfbe491fab00ea34e9212ae851742777d6f33a2e3ce90d16c89036f0eaf1aa" target="_blank">Sample</a></div>
+				-->
+			</div>
+		</div>
+		<div class="header-top">
+			<?php include '../../includes/nav.php'; ?>
+		</div>
+		<div class="white-slice al-top-content paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <p>Hello Tennessee Teachers, Welcome to your 2018 Resources site. EMC offers Spanish, French, German, and Chinese Language Programs, all of which are available in our award winning digital platform Passport<sup>&reg;</sup>.  
+				Feel free to look at our resources or go to our Tennessee Homepage for more information and to request more samples.
+				</p>
+            </div>
+        </div>
+        <div class="grey-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h3>Passport</h3>
+                <p>There is a lot that could be said about Passport, but its even better to <a href="/passport/tour/" class="learn-more-link">experience it</a>.</p>
+            </div>
+        </div>
+        <div class="white-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h3>Tennesee State Approved Language Programs</h3>
+                <p><a href="https://www.joomag.com/magazine/texas-adoption-program-overviews/M0939966001459540031" target="_blank" class="learn-more-link">See the detailed Program overview for our Spanish and French programs.</a></p>
+            </div>
+        </div>
+        <div class="grey-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h3>Available Languages Overview Brochures</h3>
+                <p><a href="https://www.joomag.com/magazine/emc-world-language-brochures/M0396129001457106036" target="_blank" class="learn-more-link">See our Program Overview Brochures here</a>.</p>
+            </div>
+        </div>
+        <div class="white-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h3>Correlations</h3>
+                <p><a href="https://drive.google.com/file/d/1s8MiJUU_eQIOigZH0tAvO3lX6Twn9bhv/view?usp=sharing" target="_blank" class="learn-more-link">French Level 2 Correlation</a></p>
+                <p><a href="https://drive.google.com/file/d/1P19FBcTyTCpcKDCutE6EpikDuHC8_lBa/view?usp=sharing" target="_blank" class="learn-more-link">French Level 3 Correlation</a></p>
+            </div>
+        </div>
+        <div class="grey-slice paddingtop20 paddingbottom20">
+            <div class="seveneighty">
+                <h3>Extended reading support</h3>
+                <p>Enhance your students' learning experience with leveled readers in the target language. Readers help develop students’ grammar and vocabulary knowledge while building their ability to comprehend increasingly difficult pieces of literature.</p>
+
+                <p><a href="/world-languages/readers/" class="learn-more-link">Click here</a> to learn more about readers.</p>
+
+            </div>
+        </div>
+    </div>
+
+<?php include '../../includes/footer.php'; ?>
