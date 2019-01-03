52b951179386bf1fb0be55f89617f7f78b4755cd
Brad Knutson <brad.s.knutson@gmail.com>
2019-01-03 11:45:21 -0600

    Old IRC redirect (old commit)

---
+++ upgrade/index.php
@@ -0,0 +1,17 @@
+<?php 
+	$custom_title = 'Upgrade | ';
+	include '../includes/header.php';
+?>
+	<div id="container" class="technology">
+		<div class="header-secondary-page"></div>
+		<div class="header-top-dark">
+			<?php include '../includes/nav.php'; ?>
+		</div>
+		<div class="world-languages-title">
+			<div class="ninehundred onesix fonttwenty">
+                <h2>The resource you were attempting to reach is no longer available.</h2>
+                <p>To learn more about currently available materials, please browse the navigation at the top of this page.</p>
+			</div>
+		</div>
+		
+<?php include '../includes/footer.php'; ?>		
