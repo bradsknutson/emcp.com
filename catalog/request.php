e83f7ac0df0e5b3529fab366a665d34867575145
Brian Molina <bmolina@emcp.com>
2019-01-07 09:11:41 -0700

    Update request form

--- catalog/request.php
+++ catalog/request.php
@@ -7,9 +7,11 @@
 	<div class="header-top-dark">
 		<?php include '../includes/nav.php'; ?>
 	</div>
-	<div class="ninesixty">
-		<iframe src="https://pages.exacttarget.com/emcsample" class="form" style="height:900px;width:100%;border:0;"></iframe>
+	<div class="ninesixty" style="margin:50px auto !important;">
+		<h2 style="text-align:center;">Request a Sample</h2>
+		<script src="//app-abd.marketo.com/js/forms2/js/forms2.min.js"></script>
+			<form id="mktoForm_1007"></form>
+		<script>MktoForms2.loadForm("//app-abd.marketo.com", "041-RWW-195", 1007);</script>
 		<div class="clearfix"></div>
 	</div>
-
 <?php include '../includes/footer.php'; ?>
