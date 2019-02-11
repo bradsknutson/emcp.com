830445d3ec18d8db92f261b1524e987f5eadaf9d
tspoto <tspoto@carnegielearning.com>
2019-02-11 10:56:39 -0500

    Update any Zulama links to point to CL.com's new Zulama page and open in new tab

--- includes/nav.php
+++ includes/nav.php
@@ -1,17 +1,17 @@
-<?php 
-    // $server is defined in server.php 
-    // get_current() is defined in functions.php 
-    // $current is defined in functions.php 
-?>			
+<?php
+    // $server is defined in server.php
+    // get_current() is defined in functions.php
+    // $current is defined in functions.php
+?>
     <div class="container">
-	
+
 		<a href="<?php echo $server; ?>"><img src="<?php echo $server; ?>lib/images/logo.png" alt="EMC" /></a>
-		
+
 		<!-- Request Button -->
 		<a href="/catalog/request.php" class="request">SAMPLE</a>
-		
+
 		<nav class="main-navigation" role="navigation">
-			
+
 			<ul id="sf" class="sf-menu">
 
                 <!--
@@ -22,7 +22,7 @@
 						<li><a href="<?php echo $server; ?>technology/iculture/">i-Culture</a></li>
 						<li><a href="<?php echo $server; ?>technology/avenue/">Avenue</a></li>
 						<li><a href="<?php echo $server; ?>technology/elpac/">ELPAC</a></li>
-					</ul>                
+					</ul>
                 </li>
 				-->
 				<li<?php echo get_current('world-languages',$current); ?>><a href="<?php echo $server; ?>world-languages/">World Languages</a>
@@ -60,11 +60,11 @@
 						<li><a href="<?php echo $server; ?>applied-learning/financial-literacy/">Financial Literacy</a></li>
 						<li><a href="<?php echo $server; ?>applied-learning/career-life-skills/">Career & Life Skills</a></li>
 					</ul>
-                
+
                 </li>
-				<li<?php echo get_current('about',$current); ?>><a href="<?php echo $server; ?>applied-learning/zulama/">Computer Literacy</a>
+				<li<?php echo get_current('about',$current); ?>><a target="_blank" href="https://www.carnegielearning.com/products/software-platform/computer-science-learning-software/">Computer Literacy</a>
 					<ul>
-						<li><a href="<?php echo $server; ?>applied-learning/zulama/">Computer Science</a></li>
+						<li><a target="_blank" href="https://www.carnegielearning.com/products/software-platform/computer-science-learning-software/">Computer Science</a></li>
 						<li><a href="<?php echo $server; ?>applied-learning/business-education/">Business Education &<br />Computer Skills</a></li>
 						<li><a href="<?php echo $server; ?>applied-learning/computer-applications/">Computer Applications</a></li>
 					</ul>
@@ -72,7 +72,7 @@
 				<li<?php echo get_current('store',$current); ?>><a href="https://store.emcp.com" title="Home">Store</a></li>
 
 			</ul><!-- end #sf -->
-			
+
 		</nav><!-- end .main-navigation -->
-	
+
 	</div><!-- end .container -->
