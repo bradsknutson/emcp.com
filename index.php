77e00700faaf381658cd819370334c29630de728
tspoto <tspoto@carnegielearning.com>
2019-04-20 18:02:37 -0400

    Remove Webinar section

--- index.php
+++ index.php
@@ -39,43 +39,6 @@
 				<div class="clearfix"></div>
 			</div>
 		</div>
-	<!-- Passport Webinar -->
-		<div class="light-grey-slice" style="padding:20px 0 0 !important;">
-            <div class="ninehundred" style="padding-top:10px !important;">
-				<p style="font-size:20px;line-height:1.6;">
-					<a href="https://calendly.com/emcpassport/demo/01-07-2019" target="_blank"><img src="/lib/images/home-2019-webinar.png" alt="" border="0" style="float:left;margin-bottom:25px;padding-right:20px;" /></a>
-					<br />
-					<strong>Join us for a live webinar:</strong><br />
-					Meet Passport<sup>&reg;</sup>, EMC's Immersive Learning Environment for English Language Arts<br /><br />
-				</p>
-				<p style="font-size:20px;line-height:1.3;" class="clearfix">
-					Join us for a professional development webinar where you will learn more about EMC's innovative learning environment, Passport<sup>&reg;</sup>.
-					In this webinar, we'll explore the tools you can use to engage students in language arts through relevant content, communication, and media literacy.
-				</p>
-				<ul style="font-size:20px;line-height:1.3;">
-					<li>Collaborative video discussions with exclusive integrated Flipgrid<sup>&reg;</sup>access
-					<li>Project-based learning
-					<li>Close Reading videos
-					<li>Extended reading and media connections
-					<li>Opportunities for differentiated and personalized instruction
-				</ul>
-				<p style="font-size:20px;line-height:1.3;">
-					<a class="mw-buttons" href="https://calendly.com/emcpassport/live-demonstration-of-passport-clone/02-05-2019" target="_blank" style="float:right;margin:30px 200px 0 0;">Register</a>
-					<strong>Schedule at a glance:</strong><br />
-
-                    When: Tuesday, February 5th, 4:00 pm CST<br />
-                    When: Thursday, February 7th, 7:00 pm CST<br />
-                    When: Tuesday, February 12th, 7:00 pm CST<br />
-                    When: Thursday, February 14th, 4:00 pm CST<br />
-                    When: Tuesday, February 19th, 4:00 pm CST<br />
-                    When: Thursday, February 21st, 7:00 pm CST<br />
-                    When: Tuesday, February 26th, 7:00 pm CST<br />
-                    When: Thursday, February 28th, 4:00 pm CST
-				</p>
-            </div>
-			<div class="clearfix"></div>
-		</div>
-	<!-- END New -->
 	<!-- Testimonial Link -->
 		<div class="white-slice" style="padding:20px 0 0 !important;">
             <div class="ninehundred" style="padding-top:10px !important;">
