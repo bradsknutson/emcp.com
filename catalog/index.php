f3cfe7397c4b135587ccd3bdbc88aba78e5db683
tspoto <tspoto@carnegielearning.com>
2019-06-10 08:09:12 -0400

    Update catelog copy

--- catalog/index.php
+++ catalog/index.php
@@ -9,7 +9,7 @@
 		<div class="white-slice light-grey-slice" style="margin-top:125px;">
 			<div class="ninesixty">
                 <div>
-					<p style="margin-top:0;">Thanks for your interest in EMC's 2018 catalogs. Scroll down to view, download or share our English Language Arts, Social Studies, World Languages, Applied Science and Applied Learning catalogs.</p>
+					<p style="margin-top:0;">Thanks for your interest in EMC's 2019 catalogs. Scroll down to view, download or share our English Language Arts and World Languages catalogs.</p>
 					<p>Questions? Please feel free to contact your <a href="https://store.emcp.com/about-emcp/local-account-manager-locator">Account Manager</a>.</p>
 				</div>
                 <div class="clearfix"></div>
