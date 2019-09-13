a4ceed798948e10c1ad5f8d6dea8039f66a3f207
tspoto <tspoto@carnegielearning.com>
2019-09-13 09:34:39 -0400

    nav/includes updates

--- includes/nav.php
+++ includes/nav.php
@@ -14,57 +14,31 @@
 
 			<ul id="sf" class="sf-menu">
 
-                <!--
-				<li<?php echo get_current('technology',$current); ?>><a href="<?php echo $server; ?>technology/">Technology</a>
-					<ul>
-						<li><a href="<?php echo $server; ?>technology/passport/">Passport</a></li>
-						<li><a href="<?php echo $server; ?>technology/ebooks/">eBooks</a></li>
-						<li><a href="<?php echo $server; ?>technology/iculture/">i-Culture</a></li>
-						<li><a href="<?php echo $server; ?>technology/avenue/">Avenue</a></li>
-						<li><a href="<?php echo $server; ?>technology/elpac/">ELPAC</a></li>
-					</ul>
-                </li>
-				-->
-				<li<?php echo get_current('world-languages',$current); ?>><a href="<?php echo $server; ?>world-languages/">World Languages</a>
+				<li<?php echo get_current('world-languages',$current); ?>><a>World Languages</a>
 					<ul>
 						<li><a href="<?php echo $server; ?>world-languages/spanish/quechevere/">Spanish</a></li>
-						<li><a href="<?php echo $server; ?>world-languages/spanish/mimundo/">Elementary Spanish</a></li>
+						<li><a href="<?php echo $server; ?>world-languages/symtalk/">Elementary Spanish</a></li>
 						<li><a href="<?php echo $server; ?>world-languages/french/tes-branche/">French</a></li>
 						<li><a href="<?php echo $server; ?>world-languages/german/deutsch-aktuell/">German</a></li>
 						<li><a href="<?php echo $server; ?>world-languages/chinese/zhen-bang/">Chinese</a></li>
 						<li><a href="<?php echo $server; ?>world-languages/italian/">Italian</a></li>
-						<li><a href="<?php echo $server; ?>technology/passport/">Passport</a></li>
 						<li><a href="<?php echo $server; ?>technology/iculture/">i-Culture</a></li>
-						<li><a href="<?php echo $server; ?>world-languages/readers/">Readers</a></li>
-						<li><a href="<?php echo $server; ?>world-languages/spanish/mimundo/index.php">Mi Mundo</a></li>
-						<li><a href="<?php echo $server; ?>world-languages/symtalk/">Symtalk</a></li>
 					</ul>
 				</li>
-				<li<?php echo get_current('language-arts',$current); ?>><a href="<?php echo $server; ?>language-arts/">Language Arts</a>
+				<li<?php echo get_current('language-arts',$current); ?>><a>Language Arts</a>
 					<ul>
 						<li><a href="<?php echo $server; ?>language-arts/mirrors-windows/">Mirrors & Windows</a></li>
 						<li><a href="<?php echo $server; ?>language-arts/mirrors-windows/passport/">Passport</a></li>
 						<li><a href="<?php echo $server; ?>language-arts/access-editions/">Access Editions</a></li>
                     </ul>
-                </li>
-				<li<?php echo get_current('applied-learning',$current); ?>><a href="<?php echo $server; ?>applied-learning/">Applied Learning</a>
+        </li>
+				<li<?php echo get_current('applied-learning',$current); ?>><a href="<?php echo $server; ?>applied-learning/applied-science/">Applied Learning</a>
 					<ul>
-						<li><a href="<?php echo $server; ?>applied-learning/applied-science/">Applied Science</a></li>
-						<li><a href="<?php echo $server; ?>applied-learning/financial-literacy/">Financial Literacy</a></li>
-						<li><a href="<?php echo $server; ?>applied-learning/career-life-skills/">Career & Life Skills</a></li>
+						<li><a href="<?php echo $server; ?>applied-learning/financial-literacy/economics/">Economics</a></li>
+						<li><a href="<?php echo $server; ?>applied-learning/applied-science/biotechnology/">Biotechnology</a></li>
+						<li><a href="<?php echo $server; ?>applied-learning/applied-science/anatomy-physiology/">Anatomy & Physiology</a></li>
 					</ul>
-
-                </li>
-				<li<?php echo get_current('about',$current); ?>><a target="_blank" href="https://www.carnegielearning.com/products/software-platform/computer-science-learning-software/">Computer Literacy</a>
-					<ul>
-						<li><a target="_blank" href="https://www.carnegielearning.com/products/software-platform/computer-science-learning-software/">Computer Science</a></li>
-						<li><a href="<?php echo $server; ?>applied-learning/business-education/">Business Education &<br />Computer Skills</a></li>
-						<li><a href="<?php echo $server; ?>applied-learning/computer-applications/">Computer Applications</a></li>
-					</ul>
-				</li>
-
+        </li>
 			</ul><!-- end #sf -->
-
 		</nav><!-- end .main-navigation -->
-
 	</div><!-- end .container -->
